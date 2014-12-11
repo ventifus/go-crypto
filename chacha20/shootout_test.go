cdf761509f2b3e1acddab922ba8f378baa4e3a2b
Coda Hale <coda.hale@gmail.com>
2014-12-11 08:07:43 -0800

    go.crypto/chacha20: add ChaCha20 implementation
    This adds an implementation of the ChaCha20 stream cipher.
    
    Change-Id: I74e8899974ca9a92279356a8b37b19c820b73d4e

---
+++ chacha20/shootout_test.go
@@ -0,0 +1,48 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package chacha20_test
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rc4"
+	"testing"
+
+	"github.com/codahale/chacha20"
+)
+
+const benchSize = 1024 * 1024
+
+func benchmarkStream(b *testing.B, c cipher.Stream) {
+	b.SetBytes(benchSize)
+	input := make([]byte, benchSize)
+	output := make([]byte, benchSize)
+	for i := 0; i < b.N; i++ {
+		c.XORKeyStream(output, input)
+	}
+}
+
+func BenchmarkChaCha20(b *testing.B) {
+	key := make([]byte, chacha20.KeySize)
+	nonce := make([]byte, chacha20.NonceSize)
+	c, _ := chacha20.New(key, nonce)
+	benchmarkStream(b, c)
+}
+
+func BenchmarkAESCTR(b *testing.B) {
+	key := make([]byte, 32)
+	a, _ := aes.NewCipher(key)
+
+	iv := make([]byte, aes.BlockSize)
+	c := cipher.NewCTR(a, iv)
+
+	benchmarkStream(b, c)
+}
+
+func BenchmarkRC4(b *testing.B) {
+	key := make([]byte, 32)
+	c, _ := rc4.NewCipher(key)
+	benchmarkStream(b, c)
+}
