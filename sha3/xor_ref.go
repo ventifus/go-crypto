9bd3b33bbaa1ccbf3785bf420718aa930d19b62c
David Leon Gil <coruus@gmail.com>
2014-12-26 19:04:10 -0800

    go.crypto/sha3: use unaligned reads and xors on x86 and x64
    Speedup of about 1.4x on x64. Added benchmarks that use the
    ShakeHash interface, which doesn't require copying the state.
    
    Substantial contributions from Eric Eisner.
    
    See golang.org/cl/151630044 for the previous CR.
    
    (There are also some minor edits/additions to the documentation.)
    
    Change-Id: I9500c25682457c82487512b9b8c66df7d75bff5d

---
+++ sha3/xor_ref.go
@@ -0,0 +1,32 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !amd64,!386,appengine
+
+package sha3
+
+import (
+	"encoding/binary"
+)
+
+// xorIn xors the bytes in buf into the state; it makes
+// no non-portable assumptions about memory layout or
+// alignment.
+func (d *state) xorIn(buf []byte) {
+	n := len(buf) / 8
+
+	for i := 0; i < n; i++ {
+		a := binary.LittleEndian.Uint64(buf)
+		d.a[i] ^= a
+		buf = buf[8:]
+	}
+}
+
+// copyOut copies ulint64s to a byte buffer.
+func (d *state) copyOut(b []byte) {
+	for i := 0; len(b) >= 8; i++ {
+		binary.LittleEndian.PutUint64(b, d.a[i])
+		b = b[8:]
+	}
+}
