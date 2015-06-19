9fc84fef70780035e73bf711ef1a6842af0c7315
Paul Meyer <paul.meyer@microsoft.com>
2015-06-18 17:47:17 -0700

    x/crypto: Add pkcs12 package for reading pkcs12 data
    Package pkcs12 provides some Go implementations of PKCS#12.
    This implementation is distilled from https://tools.ietf.org/html/rfc7292 and
    referenced documents. It is intented for decoding P12/PFX-stored certificate+key
    for use with the crypto/tls package.
    
    Package includes @dgryski's RC2 implementation as a sub package as requested in
    https://github.com/golang/go/issues/10621.
    
    Change-Id: I78401241e39cd0099e9082a3a227cf0a3a36e6d1

---
+++ pkcs12/crypto.go
@@ -0,0 +1,91 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pkcs12
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/des"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"golang.org/x/crypto/pkcs12/rc2"
+)
+
+const (
+	pbeWithSHAAnd3KeyTripleDESCBC = "pbeWithSHAAnd3-KeyTripleDES-CBC"
+	pbewithSHAAnd40BitRC2CBC      = "pbewithSHAAnd40BitRC2-CBC"
+)
+
+var algByOID = map[string]string{
+	"1.2.840.113549.1.12.1.3": pbeWithSHAAnd3KeyTripleDESCBC,
+	"1.2.840.113549.1.12.1.6": pbewithSHAAnd40BitRC2CBC,
+}
+
+var blockcodeByAlg = map[string]func(key []byte) (cipher.Block, error){
+	pbeWithSHAAnd3KeyTripleDESCBC: des.NewTripleDESCipher,
+	pbewithSHAAnd40BitRC2CBC: func(key []byte) (cipher.Block, error) {
+		return rc2.New(key, len(key)*8)
+	},
+}
+
+type pbeParams struct {
+	Salt       []byte
+	Iterations int
+}
+
+func pbDecrypterFor(algorithm pkix.AlgorithmIdentifier, password []byte) (cipher.BlockMode, error) {
+	algorithmName, supported := algByOID[algorithm.Algorithm.String()]
+	if !supported {
+		return nil, NotImplementedError("algorithm " + algorithm.Algorithm.String() + " is not supported")
+	}
+
+	var params pbeParams
+	if _, err := asn1.Unmarshal(algorithm.Parameters.FullBytes, &params); err != nil {
+		return nil, err
+	}
+
+	k := deriveKeyByAlg[algorithmName](params.Salt, password, params.Iterations)
+	iv := deriveIVByAlg[algorithmName](params.Salt, password, params.Iterations)
+	password = nil
+
+	code, err := blockcodeByAlg[algorithmName](k)
+	if err != nil {
+		return nil, err
+	}
+
+	cbc := cipher.NewCBCDecrypter(code, iv)
+	return cbc, nil
+}
+
+func pbDecrypt(info decryptable, password []byte) (decrypted []byte, err error) {
+	cbc, err := pbDecrypterFor(info.GetAlgorithm(), password)
+	password = nil
+	if err != nil {
+		return nil, err
+	}
+
+	encrypted := info.GetData()
+
+	decrypted = make([]byte, len(encrypted))
+	cbc.CryptBlocks(decrypted, encrypted)
+
+	if psLen := int(decrypted[len(decrypted)-1]); psLen > 0 && psLen < 9 {
+		m := decrypted[:len(decrypted)-psLen]
+		ps := decrypted[len(decrypted)-psLen:]
+		if bytes.Compare(ps, bytes.Repeat([]byte{byte(psLen)}, psLen)) != 0 {
+			return nil, ErrDecryption
+		}
+		decrypted = m
+	} else {
+		return nil, ErrDecryption
+	}
+
+	return
+}
+
+type decryptable interface {
+	GetAlgorithm() pkix.AlgorithmIdentifier
+	GetData() []byte
+}
---
+++ pkcs12/mac.go
@@ -0,0 +1,55 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pkcs12
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/x509/pkix"
+	"hash"
+)
+
+type macData struct {
+	Mac        digestInfo
+	MacSalt    []byte
+	Iterations int `asn1:"optional,default:1"`
+}
+
+// from PKCS#7:
+type digestInfo struct {
+	Algorithm pkix.AlgorithmIdentifier
+	Digest    []byte
+}
+
+const (
+	sha1Algorithm = "SHA-1"
+)
+
+var (
+	hashNameByID = map[string]string{
+		"1.3.14.3.2.26": sha1Algorithm,
+	}
+	hashByName = map[string]func() hash.Hash{
+		sha1Algorithm: sha1.New,
+	}
+)
+
+func verifyMac(macData *macData, message, password []byte) error {
+	name, ok := hashNameByID[macData.Mac.Algorithm.Algorithm.String()]
+	if !ok {
+		return NotImplementedError("unknown digest algorithm: " + macData.Mac.Algorithm.Algorithm.String())
+	}
+	k := deriveMacKeyByAlg[name](macData.MacSalt, password, macData.Iterations)
+	password = nil
+
+	mac := hmac.New(hashByName[name], k)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+
+	if !hmac.Equal(macData.Mac.Digest, expectedMAC) {
+		return ErrIncorrectPassword
+	}
+	return nil
+}
