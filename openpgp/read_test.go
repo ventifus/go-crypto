2769857fb7fcbc66820bd62e4aaf9ecbd6406bdd
Brian Gitonga Marete <marete@toshnix.com>
2015-07-27 01:25:02 +0300

    openpgp: Fix panic on opaque subpackets with length 0.
    Some invalid input may be parsed so that the length of an opaque
    subpacket turns out to be 0. In such cases, arrange for a
    StructuralError to be returned indicating truncation.
    
    Found using gofuzz.
    
    Fixes golang/go#11503
    
    Change-Id: Ib9ce8c604f35a31f852adfcd56a22dfd143a9443

--- openpgp/read_test.go
+++ openpgp/read_test.go
@@ -8,11 +8,12 @@ import (
 	"bytes"
 	_ "crypto/sha512"
 	"encoding/hex"
-	"golang.org/x/crypto/openpgp/errors"
 	"io"
 	"io/ioutil"
 	"strings"
 	"testing"
+
+	"golang.org/x/crypto/openpgp/errors"
 )
 
 func readerFromHex(s string) io.Reader {
@@ -365,6 +366,40 @@ func TestNoArmoredData(t *testing.T) {
 	_, err := ReadArmoredKeyRing(bytes.NewBufferString("foo"))
 	if _, ok := err.(errors.InvalidArgumentError); !ok {
 		t.Errorf("error was not an InvalidArgumentError: %s", err)
+	}
+}
+
+// An empty Keyring.
+type emptyKR struct {
+}
+
+func (kr emptyKR) KeysById(id uint64) []Key {
+	return nil
+}
+
+func (kr emptyKR) DecryptionKeys() []Key {
+	return nil
+}
+
+func (kr emptyKR) KeysByIdUsage(uint64, byte) []Key {
+	return nil
+}
+
+func TestIssue11503(t *testing.T) {
+	data := "8c040402000aa430aa8228b9248b01fc899a91197130303030"
+
+	buf, err := hex.DecodeString(data)
+	if err != nil {
+		t.Errorf("hex.DecodeSting(): %v", err)
+	}
+
+	_, err = ReadMessage(bytes.NewBuffer(buf), emptyKR{},
+		func([]Key, bool) ([]byte, error) {
+			return []byte("insecure"), nil
+		}, nil)
+
+	if err == nil {
+		t.Errorf("ReadMessage(): Unexpected nil error")
 	}
 }
 
