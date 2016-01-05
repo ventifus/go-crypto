0373ad69f4ee2debac8d5a174b7fd5bb25547f1b
Henrik Hodne <henrik@hodne.io>
2016-01-05 20:55:22 +0000

    ssh: fix height/width order in RequestPty example
    The RequestPty function takes the size arguments in the order height,
    then width, instead of the more common width, then height. 80 is a very
    common width for a terminal, so when the example reads RequestPty(...,
    80, 40, ...), it's easy to assume that the order is width-height.
    Switching the order should make it more obvious what is going on.
    
    Change-Id: I1d6266b1c0dcde5ee6e31a6d26d2dcaf14fec58a

--- ssh/example_test.go
+++ ssh/example_test.go
@@ -233,7 +233,7 @@ func ExampleSession_RequestPty() {
 		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
 	}
 	// Request pseudo terminal
-	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+	if err := session.RequestPty("xterm", 40, 80, modes); err != nil {
 		log.Fatalf("request for pseudo terminal failed: %s", err)
 	}
 	// Start remote shell
