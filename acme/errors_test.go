3cb6f5358e86e039830f5ce6342beaedace95253
Alex Vaghin <crhyme@google.com>
2016-03-18 12:12:46 +0000

    acme: initial import of ACME implementation
    Originated from github.com/google/goacme.
    
    This does not include goacme command line program
    github.com/google/goacme/cmd/goacme. It will probably live somewhere
    else.
    
    Change-Id: I852d55c1a3673fab208e6e2aa19d22be42b8719f

---
+++ acme/errors_test.go
@@ -0,0 +1,53 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package acme
+
+import (
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestResponseError(t *testing.T) {
+	err500 := &Error{Status: 500, Detail: "500 Internal"}
+	errTLS := &Error{Status: 500, Type: ErrTLS, Detail: "TLS err"}
+	errMal := &Error{Status: 409, Type: ErrMalformed, Detail: "Already in use"}
+	tests := []struct {
+		body   string
+		status string
+		code   int
+		err    *Error
+	}{
+		// won't unmarshal: should take resp.Status and .StatusCode
+		{"", "500 Internal", 500, err500},
+		// no "status" in JSON error: should take it from resp.Status
+		{`{"type":"urn:acme:error:tls","detail":"TLS err"}`, "500 Server Error", 500, errTLS},
+		// resp.StatusCode and "status" JSON field different: make sure we prefer what's in the JSON error
+		{`{"type":"urn:acme:error:malformed","detail":"Already in use","status":409}`, "400 Bad Request", 400, errMal},
+	}
+	for i, test := range tests {
+		res := &http.Response{
+			Body:       ioutil.NopCloser(strings.NewReader(test.body)),
+			Status:     test.status,
+			StatusCode: test.code,
+			Header:     http.Header{"X-Foo": {"bar"}},
+		}
+		err := responseError(res).(*Error)
+		if err.Status != test.err.Status {
+			t.Errorf("%d: err.Status = %d; want %d", i, err.Status, test.err.Status)
+		}
+		if err.Type != test.err.Type {
+			t.Errorf("%d: err.Type = %q; want %q", i, err.Type, test.err.Type)
+		}
+		if err.Detail != test.err.Detail {
+			t.Errorf("%d: err.Detail = %q; want %q", i, err.Detail, test.err.Detail)
+		}
+		if !reflect.DeepEqual(err.Header, res.Header) {
+			t.Errorf("%d: err.Header = %p; want %p", i, err.Header, res.Header)
+		}
+	}
+}
