b2ae13a7c32a6c6ec9d2b7857cbf423578acf4f2
Mark Severson <miquella@gmail.com>
2016-04-02 19:02:50 -0600

    x/crypto/ssh/agent: Support v1 remove all message
    Some ssh-agent clients expect the server to support remove all messages
    for protocols 1 & 2 and error if protocol 1 support is missing.
    
    This adds a null-op implementation of the remove all message in similar
    fashion to the existing list all message support.
    
    Fixes golang/go#15159
    
    Change-Id: I9389885d89c9147f3e10850893bba6ed7d0a4e82

--- ssh/agent/server_test.go
+++ ssh/agent/server_test.go
@@ -5,6 +5,10 @@
 package agent
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
 	"testing"
 
 	"golang.org/x/crypto/ssh"
@@ -17,11 +21,11 @@ func TestServer(t *testing.T) {
 	}
 	defer c1.Close()
 	defer c2.Close()
-	client := NewClient(c1)
+	c := NewClient(c1)
 
 	go ServeAgent(NewKeyring(), c2)
 
-	testAgentInterface(t, client, testPrivateKeys["rsa"], nil, 0)
+	testAgentInterface(t, c, testPrivateKeys["rsa"], nil, 0)
 }
 
 func TestLockServer(t *testing.T) {
@@ -74,4 +78,59 @@ func TestSetupForwardAgent(t *testing.T) {
 	agentClient := NewClient(ch)
 	testAgentInterface(t, agentClient, testPrivateKeys["rsa"], nil, 0)
 	conn.Close()
+}
+
+func TestV1ProtocolMessages(t *testing.T) {
+	c1, c2, err := netPipe()
+	if err != nil {
+		t.Fatalf("netPipe: %v", err)
+	}
+	defer c1.Close()
+	defer c2.Close()
+	c := NewClient(c1)
+
+	go ServeAgent(NewKeyring(), c2)
+
+	testV1ProtocolMessages(t, c.(*client))
+}
+
+func testV1ProtocolMessages(t *testing.T, c *client) {
+	err := testV1ProtocolMessage(c, []byte{agentRequestV1Identities}, ssh.Marshal(agentV1IdentityMsg{0}))
+	if err != nil {
+		t.Fatalf("v1 request all failed: %v", err)
+	}
+
+	err = testV1ProtocolMessage(c, []byte{agentRemoveAllV1Identities}, []byte{agentSuccess})
+	if err != nil {
+		t.Fatalf("v1 remove all failed: %v", err)
+	}
+}
+
+func testV1ProtocolMessage(c *client, req, resp []byte) error {
+	var err error
+	msg := make([]byte, 4+len(req))
+	binary.BigEndian.PutUint32(msg, uint32(len(req)))
+	copy(msg[4:], req)
+	if _, err = c.conn.Write(msg); err != nil {
+		return clientErr(err)
+	}
+
+	var respSizeBuf [4]byte
+	if _, err = io.ReadFull(c.conn, respSizeBuf[:]); err != nil {
+		return clientErr(err)
+	}
+	respSize := binary.BigEndian.Uint32(respSizeBuf[:])
+	if respSize > maxAgentResponseBytes {
+		return clientErr(err)
+	}
+
+	buf := make([]byte, respSize)
+	if _, err = io.ReadFull(c.conn, buf); err != nil {
+		return clientErr(err)
+	}
+
+	if !bytes.Equal(buf, resp) {
+		return fmt.Errorf("invalid response. expected: %v; got: %v", resp, buf)
+	}
+	return nil
 }
