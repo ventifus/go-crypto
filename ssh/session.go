2900c1648f555e23b2224396f9a887b6e79b5989
John Schnake <schnake.john@gmail.com>
2016-05-11 13:04:32 -0500

    ssh: Add window resizing messages
    RFC 4254 Section 6.7 describes this message as optional and does
    not require a response.
    
    Fixes golang/go#15649
    
    Change-Id: Iae94feeab34f652b0ce68d010ea7e348a154e0cb

--- ssh/session.go
+++ ssh/session.go
@@ -230,6 +230,26 @@ func (s *Session) RequestSubsystem(subsystem string) error {
 	return err
 }
 
+// RFC 4254 Section 6.7.
+type windowDimensionChangeMsg struct {
+	Columns uint32
+	Rows    uint32
+	Width   uint32
+	Height  uint32
+}
+
+// WindowDimensionChange informs the remote host of a terminal size change on the client.
+func (s *Session) WindowDimensionChange(h, w int) error {
+	req := windowDimensionChangeMsg{
+		Columns: uint32(w),
+		Rows:    uint32(h),
+		Width:   uint32(w * 8),
+		Height:  uint32(h * 8),
+	}
+	_, err := s.ch.SendRequest("window-change", false, Marshal(&req))
+	return err
+}
+
 // RFC 4254 Section 6.9.
 type signalMsg struct {
 	Signal string
