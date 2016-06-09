0e706f4e45c3f7557f927ddf2f80fd182bc21cb1
Alex Vaghin <ddos@google.com>
2016-06-09 20:11:16 +0100

    acme: minimal set for the manager API
    The plan is to design a higher level API surface w.r.t. acme.Client.
    This is the first such attempt.
    
    It is heavily based on the ideas from rsc.io/letsencrypt.
    
    Change-Id: I62021452a918cd49093162f3d6c74e9d7f452fb8

---
+++ acme/internal/manager/man.go
@@ -0,0 +1,55 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package manager provides high-level ACME-based certificates management,
+// built on top of the acme package.
+package manager
+
+import (
+	"crypto/tls"
+	"errors"
+)
+
+const letsEncryptURL = "https://acme-v01.api.letsencrypt.org/directory"
+
+// Manager is a stateful certificates manager built on top of acme.Client.
+// It obtains new and refreshes expiring certificates automatically,
+// as well as providing them to a TLS server via tls.Config.
+//
+// A zero value Manager is a valid state, in which case Let's Encrypt CA is used
+// with a newly generated RSA account key of 2048 bit size and a local dir-based cache.
+type Manager struct {
+	Client *Client
+	Cache  Cache
+}
+
+// GetCertificate fits well with tls.Config's GetCertificate field.
+// It provides a TLS certificate for hello.ServerName host, including *.acme.invalid.
+// All other fields of hello are ignored.
+//
+// A simple usage can be shown as follows:
+//
+//	s := &http.Server{
+//		Addr: ":https",
+//		TLSConfig: &tls.Config{
+//			GetCertificate: m.GetCertificate,
+//		},
+//	}
+//	s.ListenAndServeTLS("", "")
+//
+func (m *Manager) GetCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	return nil, errors.New("not implemented")
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+// It returns m's internal state representation.
+func (m *Manager) MarshalBinary() ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+// It resets m to a state encoded in data.
+func (m *Manager) UnmarshalBinary(data []byte) error {
+	return errors.New("not implemented")
+}
