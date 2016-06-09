a736fa85332da4d8441c00518ad9538ccfe8afa2
Alex Vaghin <ddos@google.com>
2016-06-09 20:11:16 +0100

    acme: minimal set for the manager API
    The plan is to design a higher level API surface w.r.t. acme.Client.
    This is the first such attempt.
    
    It is heavily based on the ideas from rsc.io/letsencrypt.
    
    Change-Id: I62021452a918cd49093162f3d6c74e9d7f452fb8

---
+++ acme/internal/acme/man.go
@@ -0,0 +1,80 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package acme
+
+import (
+	"crypto/tls"
+	"errors"
+)
+
+const letsEncryptURL = "https://acme-v01.api.letsencrypt.org/directory"
+
+// Manager is a stateful certificates manager built on top of the Client.
+// It obtains new and refreshes expiring certificates automatically,
+// as well as providing them to a TLS server via tls.Config.
+type Manager struct {
+	client *Client
+	disco  string
+}
+
+// NewManager initializes a Manager using the given client.
+// The client.Key must not be nil and is expected to be registered with the CA.
+// If the directory URL discoURL is empty, Let's Encrypt CA is used.
+func NewManager(client *Client, discoURL string) *Manager {
+	m := &Manager{
+		client: client,
+		disco:  discoURL,
+	}
+	if m.disco == "" {
+		m.disco = letsEncryptURL
+	}
+	return m
+}
+
+// Watch will call function f whenever m's state is changed.
+// Caching m's state is a typical use case. For instance,
+//
+// 	func dump(m *Manager) {
+//		b, err := m.MarshalBinary()
+//		if err != nil {
+//			log.Printf("dump: %v", err)
+//			return
+//		}
+//		ioutil.WriteFile("cache.bin", b, 0600)
+//	}
+//	m.Watch(dump)
+//
+// Watch allows registering multiple functions, at any time.
+func (m *Manager) Watch(f func(*Manager)) {
+	// not implemented
+}
+
+// GetCertificate fits well with tls.Config's GetCertificate field.
+// It provides a TLS certificate for hello.ServerName host, including *.acme.invalid.
+// All other fields of hello are ignored.
+//
+// A simple usage can be shown as follows:
+//
+//	s := &http.Server{
+//		Addr: ":https",
+//		TLSConfig: &tls.Config{
+//			GetCertificate: m.GetCertificate,
+//		},
+//	}
+//	s.ListenAndServeTLS("", "")
+//
+func (m *Manager) GetCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	return nil, errors.New("not implemented")
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+func (m *Manager) MarshalBinary() ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+func (m *Manager) UnmarshalBinary(data []byte) error {
+	return errors.New("not implemented")
+}
