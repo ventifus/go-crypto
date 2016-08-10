008450da3299d520b148bbdb570853bee11f8377
Andreas Auernhammer <aead@mail.de>
2016-08-10 12:48:33 +0200

    poly1305, chacha20: 1) Add ChaCha20 2) Add ChaCha20Poly1305 AEAD 3) Speed-up poly1305
    Add chacha20 package implementing the ChaCha20 stream cipher and the ChaCha20Poly1305
    AEAD construction specified in RFC 7539.
    Add chacha20/chacha package implemeting low level functions of the ChaCha stream
    cipher fammily. (Similar to the existing salsa20 and salsa20/salsa packages)
    
    Add Hash struct to poly1305 package to process messages with unknown length.
    (See chacha20 package)
    Improve performance of poly1305 package - speedup: round about x2 on amd64:
    ```
    name           old speed      new speed      delta
    64-4            663MB/s ± 1%  1678MB/s ± 0%   ~     (p=0.100 n=3+3)
    1K-4           1.13GB/s ± 0%  2.56GB/s ± 0%   ~     (p=0.100 n=3+3)
    64Unaligned-4   666MB/s ± 0%  1693MB/s ± 0%   ~     (p=0.100 n=3+3)
    1KUnaligned-4  1.13GB/s ± 1%  2.56GB/s ± 0%   ~     (p=0.100 n=3+3)
    
    name              speed
    Cipher64-4         586MB/s ± 1%
    Cipher1K-4        1.87GB/s ± 1%
    XORKeyStream64-4   411MB/s ± 0%
    XORKeyStream1K-4  1.72GB/s ± 0%
    Seal64B-4          172MB/s ± 1%
    Seal1K-4           870MB/s ± 0%
    Open64B-4          165MB/s ± 1%
    Open1K-4           849MB/s ± 0%
    
    ```
    
    Change-Id: Icef63441fc7c234f3a424d48dee11c60635b4dca

--- poly1305/poly1305.go
+++ poly1305/poly1305.go
@@ -2,26 +2,29 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*
-Package poly1305 implements Poly1305 one-time message authentication code as specified in http://cr.yp.to/mac/poly1305-20050329.pdf.
-
-Poly1305 is a fast, one-time authentication function. It is infeasible for an
-attacker to generate an authenticator for a message without the key. However, a
-key must only be used for a single message. Authenticating two different
-messages with the same key allows an attacker to forge authenticators for other
-messages with the same key.
-
-Poly1305 was originally coupled with AES in order to make Poly1305-AES. AES was
-used with a fixed key in order to generate one-time keys from an nonce.
-However, in this package AES isn't used and the one-time key is specified
-directly.
-*/
+// Package poly1305 implements Poly1305 one-time message authentication code as specified in http://cr.yp.to/mac/poly1305-20050329.pdf.
+//
+// Poly1305 is a fast, one-time authentication function. It is infeasible for an
+// attacker to generate an authenticator for a message without the key. However, a
+// key must only be used for a single message. Authenticating two different
+// messages with the same key allows an attacker to forge authenticators for other
+// messages with the same key.
+//
+// Poly1305 was originally coupled with AES in order to make Poly1305-AES. AES was
+// used with a fixed key in order to generate one-time keys from an nonce.
+// However, in this package AES isn't used and the one-time key is specified
+// directly.
 package poly1305 // import "golang.org/x/crypto/poly1305"
 
-import "crypto/subtle"
+import (
+	"crypto/subtle"
+	"errors"
+)
 
-// TagSize is the size, in bytes, of a poly1305 authenticator.
+// TagSize is the size of the poly1305 authentication tag in bytes.
 const TagSize = 16
+
+var errWriteAfterSum = errors.New("checksum already computed - adding more data is not allowed")
 
 // Verify returns true if mac is a valid authenticator for m with the given
 // key.
