1b30e548b54d0f867b9528ab4c85fac3476c3110
Kevin Burke <kev@inburke.com>
2016-08-10 20:25:16 -0700

    nacl/secretbox: add Seal, Open example
    I read the docs and wasn't sure how to use the package - what is
    the best way to generate a nonce? Do I need to use the same nonce
    for encrypting and decrypting the same message? I read through some
    code that used this package, and the tests, found the answers I was
    looking for, and used them to generate an example, which hopefully
    will be helpful to other users of this package.
    
    Change-Id: I24f9682fecb6632f34b4d8d13fed6eb002993677

---
+++ nacl/secretbox/secretbox_example_test.go
@@ -0,0 +1,31 @@
+package secretbox
+
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+func Example() {
+	// Load this key from a safe place and reuse it across multiple Seal calls.
+	var secretkey [32]byte
+	copy(secretkey[:], "change this password to a secret")
+
+	// You must use a different nonce for each call to Seal.
+	nonceSlice := make([]byte, 24)
+	rand.Read(nonceSlice)
+	var nonce [24]byte
+	copy(nonce[:], nonceSlice)
+
+	// This encrypts "hello world" and appends the result to the nonce.
+	encrypted := Seal(nonceSlice, []byte("hello world"), &nonce, &secretkey)
+
+	// When you decrypt, you must use the same nonce and key you used to
+	// encrypt the message. One way to achieve this is to store the nonce
+	// alongside the encrypted message. Above, we stored the nonce in the first
+	// 24 bytes of the encrypted text.
+	var decryptNonce [24]byte
+	copy(decryptNonce[:], encrypted[:24])
+	decrypted, _ := Open([]byte{}, encrypted[24:], &decryptNonce, &secretkey)
+	fmt.Println(string(decrypted))
+	// Output: hello world
+}
