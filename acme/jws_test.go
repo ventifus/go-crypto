254c84b6c5f5b632e1e70f0091feb9dfad09c90a
Matthew Endsley <mendsley@gmail.com>
2016-12-16 22:11:23 +0000

    acme: Properly support ES512 signatures
    The correct curve name for ES512 signatures is P-521 not P-512
    
    Fixes #18357
    
    Change-Id: I783033731c2b7eb8557ccf988139021d56e7605b

--- acme/jws_test.go
+++ acme/jws_test.go
@@ -73,6 +73,18 @@ var (
 	testKeyEC *ecdsa.PrivateKey
 )
 
+func parseECKey(keyPEM string) *ecdsa.PrivateKey {
+	d, _ := pem.Decode([]byte(keyPEM))
+	if d == nil {
+		panic("no block found in testKeyECPEM")
+	}
+	key, err := x509.ParseECPrivateKey(d.Bytes)
+	if err != nil {
+		panic(err.Error())
+	}
+	return key
+}
+
 func init() {
 	d, _ := pem.Decode([]byte(testKeyPEM))
 	if d == nil {
@@ -84,13 +96,7 @@ func init() {
 		panic(err.Error())
 	}
 
-	if d, _ = pem.Decode([]byte(testKeyECPEM)); d == nil {
-		panic("no block found in testKeyECPEM")
-	}
-	testKeyEC, err = x509.ParseECPrivateKey(d.Bytes)
-	if err != nil {
-		panic(err.Error())
-	}
+	testKeyEC = parseECKey(testKeyECPEM)
 }
 
 func TestJWSEncodeJSON(t *testing.T) {
@@ -141,50 +147,122 @@ func TestJWSEncodeJSON(t *testing.T) {
 }
 
 func TestJWSEncodeJSONEC(t *testing.T) {
-	claims := struct{ Msg string }{"Hello JWS"}
+	keys := []struct {
+		PEM  string
+		PubX string
+		PubY string
 
-	b, err := jwsEncodeJSON(claims, testKeyEC, "nonce")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var jws struct{ Protected, Payload, Signature string }
-	if err := json.Unmarshal(b, &jws); err != nil {
-		t.Fatal(err)
+		Alg string
+		Crv string
+	}{
+		{
+			// openssl ecparam -name secp256k1 -genkey -noout
+			`
+-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIK07hGLr0RwyUdYJ8wbIiBS55CjnkMD23DWr+ccnypWLoAoGCCqGSM49
+AwEHoUQDQgAE5lhEug5xK4xBDZ2nAbaxLtaLiv85bxJ7ePd1dkO23HThqIrvawF5
+QAaS/RNouybCiRhRjI3EaxLkQwgrCw0gqQ==
+-----END EC PRIVATE KEY-----
+`,
+			// 1. opnessl ec -in key.pem -noout -text
+			// 2. remove first byte, 04 (the header); the rest is X and Y
+			// 3. covert each with: echo <val> | xxd -r -p | base64 | tr -d '=' | tr '/+' '_-'
+			"5lhEug5xK4xBDZ2nAbaxLtaLiv85bxJ7ePd1dkO23HQ",
+			"4aiK72sBeUAGkv0TaLsmwokYUYyNxGsS5EMIKwsNIKk",
+
+			"ES256",
+			"P-256",
+		},
+		{
+			// openssl ecparam -name secp384r1 -genkey -noout
+			`
+-----BEGIN EC PRIVATE KEY-----
+MIGkAgEBBDAQ4lNtXRORWr1bgKR1CGysr9AJ9SyEk4jiVnlUWWUChmSNL+i9SLSD
+Oe/naPqXJ6CgBwYFK4EEACKhZANiAAQzKtj+Ms0vHoTX5dzv3/L5YMXOWuI5UKRj
+JigpahYCqXD2BA1j0E/2xt5vlPf+gm0PL+UHSQsCokGnIGuaHCsJAp3ry0gHQEke
+WYXapUUFdvaK1R2/2hn5O+eiQM8YzCg=
+-----END EC PRIVATE KEY-----
+`,
+			// 1. opnessl ec -in key.pem -noout -text
+			// 2. remove first byte, 04 (the header); the rest is X and Y
+			// 3. covert each with: echo <val> | xxd -r -p | base64 | tr -d '=' | tr '/+' '_-'
+			"MyrY_jLNLx6E1-Xc79_y-WDFzlriOVCkYyYoKWoWAqlw9gQNY9BP9sbeb5T3_oJt",
+			"Dy_lB0kLAqJBpyBrmhwrCQKd68tIB0BJHlmF2qVFBXb2itUdv9oZ-TvnokDPGMwo",
+
+			"ES384",
+			"P-384",
+		},
+		{
+			// openssl ecparam -name secp521r1 -genkey -noout
+			`
+-----BEGIN EC PRIVATE KEY-----
+MIHcAgEBBEIBSNZKFcWzXzB/aJClAb305ibalKgtDA7+70eEkdPt28/3LZMM935Z
+KqYHh/COcxuu3Kt8azRAUz3gyr4zZKhlKUSgBwYFK4EEACOhgYkDgYYABAHUNKbx
+7JwC7H6pa2sV0tERWhHhB3JmW+OP6SUgMWryvIKajlx73eS24dy4QPGrWO9/ABsD
+FqcRSkNVTXnIv6+0mAF25knqIBIg5Q8M9BnOu9GGAchcwt3O7RDHmqewnJJDrbjd
+GGnm6rb+NnWR9DIopM0nKNkToWoF/hzopxu4Ae/GsQ==
+-----END EC PRIVATE KEY-----
+`,
+			// 1. opnessl ec -in key.pem -noout -text
+			// 2. remove first byte, 04 (the header); the rest is X and Y
+			// 3. covert each with: echo <val> | xxd -r -p | base64 -w 100 | tr -d '=' | tr '/+' '_-'
+			"AdQ0pvHsnALsfqlraxXS0RFaEeEHcmZb44_pJSAxavK8gpqOXHvd5Lbh3LhA8atY738AGwMWpxFKQ1VNeci_r7SY",
+			"AXbmSeogEiDlDwz0Gc670YYByFzC3c7tEMeap7CckkOtuN0Yaebqtv42dZH0MiikzSco2ROhagX-HOinG7gB78ax",
+
+			"ES512",
+			"P-521",
+		},
 	}
 
-	if b, err = base64.RawURLEncoding.DecodeString(jws.Protected); err != nil {
-		t.Fatalf("jws.Protected: %v", err)
-	}
-	var head struct {
-		Alg   string
-		Nonce string
-		JWK   struct {
-			Crv string
-			Kty string
-			X   string
-			Y   string
-		} `json:"jwk"`
-	}
-	if err := json.Unmarshal(b, &head); err != nil {
-		t.Fatalf("jws.Protected: %v", err)
-	}
-	if head.Alg != "ES256" {
-		t.Errorf("head.Alg = %q; want ES256", head.Alg)
-	}
-	if head.Nonce != "nonce" {
-		t.Errorf("head.Nonce = %q; want nonce", head.Nonce)
-	}
-	if head.JWK.Crv != "P-256" {
-		t.Errorf("head.JWK.Crv = %q; want P-256", head.JWK.Crv)
-	}
-	if head.JWK.Kty != "EC" {
-		t.Errorf("head.JWK.Kty = %q; want EC", head.JWK.Kty)
-	}
-	if head.JWK.X != testKeyECPubX {
-		t.Errorf("head.JWK.X = %q; want %q", head.JWK.X, testKeyECPubX)
-	}
-	if head.JWK.Y != testKeyECPubY {
-		t.Errorf("head.JWK.Y = %q; want %q", head.JWK.Y, testKeyECPubY)
+	for _, tt := range keys {
+
+		claims := struct{ Msg string }{"Hello JWS"}
+
+		testKey := parseECKey(tt.PEM)
+
+		b, err := jwsEncodeJSON(claims, testKey, "nonce")
+		if err != nil {
+			t.Fatal(err)
+		}
+		var jws struct{ Protected, Payload, Signature string }
+		if err := json.Unmarshal(b, &jws); err != nil {
+			t.Fatal(err)
+		}
+
+		if b, err = base64.RawURLEncoding.DecodeString(jws.Protected); err != nil {
+			t.Fatalf("jws.Protected: %v", err)
+		}
+		var head struct {
+			Alg   string
+			Nonce string
+			JWK   struct {
+				Crv string
+				Kty string
+				X   string
+				Y   string
+			} `json:"jwk"`
+		}
+		if err := json.Unmarshal(b, &head); err != nil {
+			t.Fatalf("jws.Protected: %v", err)
+		}
+		if head.Alg != tt.Alg {
+			t.Errorf("head.Alg = %q; want %q", head.Alg, tt.Alg)
+		}
+		if head.Nonce != "nonce" {
+			t.Errorf("head.Nonce = %q; want nonce", head.Nonce)
+		}
+		if head.JWK.Crv != tt.Crv {
+			t.Errorf("head.JWK.Crv = %q; want %q", head.JWK.Crv, tt.Crv)
+		}
+		if head.JWK.Kty != "EC" {
+			t.Errorf("head.JWK.Kty = %q; want EC", head.JWK.Kty)
+		}
+		if head.JWK.X != tt.PubX {
+			t.Errorf("head.JWK.X = %q; want %q", head.JWK.X, tt.PubX)
+		}
+		if head.JWK.Y != tt.PubY {
+			t.Errorf("head.JWK.Y = %q; want %q", head.JWK.Y, tt.PubY)
+		}
 	}
 }
 
