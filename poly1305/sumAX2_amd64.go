fcb2ca220cb869a5846b9df5c036d5a01bdc3a02
Andreas Auernhammer <aead@mail.de>
2017-01-02 13:53:55 +0100

    poly1305: add amd64 BMI2 (AVX2) implementation
    name 		old time/op 	new time/op 	delta
    64-4 		29.7ns ± 0% 	28.8ns ± 0% 	-3.14% (p=0.100 n=3+3)
    1K-4 		399ns ± 0% 	393ns ± 0% 	-1.59% (p=0.100 n=3+3)
    64Unaligned-4 	30.1ns ± 1% 	29.3ns ± 0% 	-2.65% (p=0.100 n=3+3)
    1KUnaligned-4 	399ns ± 0% 	392ns ± 0% 	-1.75% (p=0.100 n=3+3)
    
    name 		old speed 	new speed	delta
    64-4 		2.15GB/s ± 0% 	2.22GB/s ± 0% 	+3.19% (p=0.100 n=3+3)
    1K-4 		2.57GB/s ± 0% 	2.61GB/s ± 0% 	+1.56% (p=0.100 n=3+3)
    64Unaligned-4 	2.12GB/s ± 2% 	2.18GB/s ± 0% 	+2.69% (p=0.100 n=3+3)
    1KUnaligned-4  	2.56GB/s ± 0%  	2.61GB/s ± 0%  	+1.70%  (p=0.100 n=3+3)
    
    Change-Id: I1ab5b6ffb8a47d77fe21cd4e1a3395a83fd64f4b

--- poly1305/sumAX2_amd64.go
+++ poly1305/sumAX2_amd64.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64,!go1.7,!gccgo,!appengine
+// +build go1.7,amd64,!gccgo,!appengine
 
 package poly1305
 
-// This function is implemented in sum_amd64.s
+// This function is implemented in sumAVX2_amd64.s
 //go:noescape
 func poly1305(out *[16]byte, m *byte, mlen uint64, key *[32]byte)
 
