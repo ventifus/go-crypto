dd97dfa41a5b806a15996ca27e443bdff91b9096
Andreas Auernhammer <aead@mail.de>
2017-01-02 13:53:55 +0100

    poly1305: add amd64 BMI2 (AVX2) implementation
    name 		old time/op 	new time/op 	delta
    64-4 		29.7ns ± 0% 	28.8ns ± 0% 	-3.14% (p=0.100 n=3+3)
    1K-4 		399ns ± 0% 	393ns ± 0% 	-1.59% (p=0.100 n=3+3)
    64Unaligned-4 	30.1ns ± 1% 	29.3ns ± 0% 	-2.65% (p=0.100 n=3+3)
    1KUnaligned-4 	399ns ± 0% 	392ns ± 0% 	-1.75% (p=0.100 n=3+3)
    
    name 		old speed 	new speed	delta
    64-4 		2.15GB/s ± 0% 	2.22GB/s ± 0% 	+3.19% (p=0.100 n=3+3)
    1K-4 		2.57GB/s ± 0% 	2.61GB/s ± 0% 	+1.56% (p=0.100 n=3+3)
    64Unaligned-4 	2.12GB/s ± 2% 	2.18GB/s ± 0% 	+2.69% (p=0.100 n=3+3)
    1KUnaligned-4  	2.56GB/s ± 0%  	2.61GB/s ± 0%  	+1.70%  (p=0.100 n=3+3)
    
    Change-Id: I1ab5b6ffb8a47d77fe21cd4e1a3395a83fd64f4b

--- poly1305/sumAVX2_amd64.s
+++ poly1305/sumAVX2_amd64.s
@@ -1,8 +1,8 @@
-// Copyright 2012 The Go Authors. All rights reserved.
+// Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64,!go1.7,!gccgo,!appengine
+// +build go1.7,amd64,!gccgo,!appengine
 
 #include "textflag.h"
 
@@ -54,6 +54,39 @@
 	ADCQ  t3, h1;                  \
 	ADCQ  $0, h2
 
+#define POLY1305_MUL_AVX2(h0, h1, h2, r0, r1, t0, t1, t2, t3, t4) \
+	MOVQ  r0, DX;                  \
+	MULXQ h0, t0, t1;              \
+	MULXQ h1, t3, t2;              \
+	IMULQ h2, DX;                  \
+	ADDQ  t3, t1;                  \
+	ADCQ  DX, t2;                  \
+	                               \
+	MOVQ  r1, DX;                  \
+	MULXQ h0, t3, t4;              \
+	ADDQ  t3, t1;                  \
+	ADCQ  t4, t2;                  \
+	MULXQ h1, t3, t4;              \
+	IMULQ h2, DX;                  \
+	ADDQ  t3, t2;                  \
+	ADCQ  t4, DX;                  \
+	ADCQ  $0, DX;                  \
+	                               \
+	MOVQ  t0, h0;                  \
+	MOVQ  t1, h1;                  \
+	MOVQ  t2, h2;                  \
+	ANDQ  $3, h2;                  \
+	MOVQ  t2, t0;                  \
+	ANDQ  $0XFFFFFFFFFFFFFFFC, t0; \
+	ADDQ  t0, h0;                  \
+	ADCQ  DX, h1;                  \
+	ADCQ  $0, h2;                  \
+	SHRQ  $2, DX, t2;              \
+	SHRQ  $2, DX;                  \
+	ADDQ  t2, h0;                  \
+	ADCQ  DX, h1;                  \
+	ADCQ  $0, h2
+
 DATA poly1305Mask<>+0x00(SB)/8, $0x0FFFFFFC0FFFFFFF
 DATA poly1305Mask<>+0x08(SB)/8, $0x0FFFFFFC0FFFFFFC
 GLOBL poly1305Mask<>(SB), RODATA, $16
@@ -73,8 +106,33 @@ TEXT ·poly1305(SB), $0-32
 	XORQ R9, R9                    // h1
 	XORQ R10, R10                  // h2
 
+	XORQ BP, BP
+	CMPB runtime·support_avx2(SB), $1
+	JNE no_avx2
+
+	// check BMI2 bit for MULXQ
+	// runtime·cpuid_ebx7 is always available here
+	// because it passed avx2 check
+	MOVL runtime·cpuid_ebx7(SB), BP
+	SHLQ $8, BP		// bit 8 is BMI2
+
+no_avx2:
 	CMPQ R15, $16
 	JB   bytes_between_0_and_15
+
+	TESTQ BP, BP
+	JZ   loop
+
+loop_bmi2:
+	POLY1305_ADD(SI, R8, R9, R10)
+
+multiply_bmi2:
+	POLY1305_MUL_AVX2(R8, R9, R10, R11, R12, BX, CX, R13, R14, BP)
+	SUBQ $16, R15
+	CMPQ R15, $16
+	JAE  loop_bmi2
+	MOVQ $1, BP                 // set BMI2 to true again
+	JMP  bytes_between_0_and_15
 
 loop:
 	POLY1305_ADD(SI, R8, R9, R10)
@@ -102,11 +160,13 @@ flush_buffer:
 	DECQ R15
 	JNZ  flush_buffer
 
-	ADDQ BX, R8
-	ADCQ CX, R9
-	ADCQ $0, R10
-	MOVQ $16, R15
-	JMP  multiply
+	ADDQ  BX, R8
+	ADCQ  CX, R9
+	ADCQ  $0, R10
+	MOVQ  $16, R15
+	TESTQ BP, BP
+	JNZ   multiply_bmi2
+	JMP   multiply
 
 done:
 	MOVQ    R8, AX
