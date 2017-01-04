620e8b9f713b4a973c133dd3ca5acacb191cd906
Lion Yang <lion@aosc.xyz>
2017-01-05 07:06:24 +0800

    x/crypto/chacha20poly1305: fix detection of BMI on AMD64
    This change uses runtime.support_bmi2 as an additional condition
    to examine the usability of AVX2 version algorithm, fixes
    the crash on the platfrom which supports AVX2 but not support BMI2.
    
    Change-Id: I5438d4ec84265c79a51c1439265a33b1be04878a

--- chacha20poly1305/chacha20poly1305_amd64.s
+++ chacha20poly1305/chacha20poly1305_amd64.s
@@ -279,8 +279,11 @@ TEXT ·chacha20Poly1305Open(SB), 0, $288-97
 
 	// Check for AVX2 support
 	CMPB runtime·support_avx2(SB), $1
-	JE   chacha20Poly1305Open_AVX2
-
+	JNE  noavx2bmi2Open
+	CMPB runtime·support_bmi2(SB), $1  // for MULXQ
+	JNE  noavx2bmi2Open
+	JMP   chacha20Poly1305Open_AVX2
+noavx2bmi2Open:
 	// Special optimization, for very short buffers
 	CMPQ inl, $128
 	JBE  openSSE128 // About 16% faster
@@ -1486,8 +1489,11 @@ TEXT ·chacha20Poly1305Seal(SB), 0, $288-96
 
 	// Check for AVX2 support
 	CMPB runtime·support_avx2(SB), $1
-	JE   chacha20Poly1305Seal_AVX2
-
+	JNE  noavx2bmi2Seal
+	CMPB runtime·support_bmi2(SB), $1  // for MULXQ
+	JNE  noavx2bmi2Seal
+	JMP  chacha20Poly1305Seal_AVX2
+noavx2bmi2Seal:
 	// Special optimization, for very short buffers
 	CMPQ inl, $128
 	JBE  sealSSE128 // About 15% faster
