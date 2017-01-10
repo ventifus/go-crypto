32b8f45146f1ea0c78d9471f5f13a15ddbd7bce3
Han-Wen Nienhuys <hanwen@google.com>
2017-01-10 17:30:15 +0100

    ssh: rewrite (re)keying logic.
    Use channels and a dedicated write loop for managing the rekeying
    process.  This lets us collect packets to be written while a key
    exchange is in progress.
    
    Previously, the read loop ran the key exchange, and writers would
    block if a key exchange was going on. If a reader writes
    back a packet while processing a read packet, it could block, stopping
    the read loop, thus causing a deadlock.
    
    Unfortunately, such coupled read/writes are inherent with handling
    requests that want a response (eg. keepalive, opening/closing
    channels etc.)
    
    The new scheme means that writePacket always returns immediately, so
    there is no backpressure on fast writers. Since SSH has flow control
    for normal streams, this is only an issue for processes that send OOB
    requests messages as fast as they can.
    
    Fixes #18439.
    
    Change-Id: I7c14ff4991fa3100a5d36025125d0cf1119c471d

--- ssh/client_auth.go
+++ ssh/client_auth.go
@@ -30,8 +30,13 @@ func (c *connection) clientAuthenticate(config *ClientConfig) error {
 	// then any untried methods suggested by the server.
 	tried := make(map[string]bool)
 	var lastMethods []string
+
+	sessionID := c.transport.getSessionID()
+	if err != nil {
+		return err
+	}
 	for auth := AuthMethod(new(noneAuth)); auth != nil; {
-		ok, methods, err := auth.auth(c.transport.getSessionID(), config.User, c.transport, config.Rand)
+		ok, methods, err := auth.auth(sessionID, config.User, c.transport, config.Rand)
 		if err != nil {
 			return err
 		}
