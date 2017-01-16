9f6317756342d000b825c66f338ee29ca04080fc
Han-Wen Nienhuys <hanwen@google.com>
2017-01-16 13:25:54 +0100

    ssh: soft code internal channel size for testing purposes
    Change-Id: I2ee0ed4ba82d2d156a7896551dea04b28cdeceb0

--- ssh/handshake_test.go
+++ ssh/handshake_test.go
@@ -249,6 +249,22 @@ func TestHandshakeAutoRekeyWrite(t *testing.T) {
 	defer trC.Close()
 	defer trS.Close()
 
+	done := make(chan int, 1)
+	go func() {
+		defer close(done)
+		j := 0
+		for ; j < 5; j++ {
+			_, err := trS.readPacket()
+			if err != nil {
+				break
+			}
+		}
+
+		if j != 5 {
+			t.Errorf("got %d, want 5 messages", j)
+		}
+	}()
+
 	<-checker.called
 
 	for i := 0; i < 5; i++ {
@@ -263,18 +279,7 @@ func TestHandshakeAutoRekeyWrite(t *testing.T) {
 		}
 
 	}
-
-	j := 0
-	for ; j < 5; j++ {
-		_, err := trS.readPacket()
-		if err != nil {
-			break
-		}
-	}
-
-	if j != 5 {
-		t.Errorf("got %d, want 5 messages", j)
-	}
+	<-done
 }
 
 type syncChecker struct {
@@ -305,12 +310,19 @@ func TestHandshakeAutoRekeyRead(t *testing.T) {
 	if err := trS.writePacket(packet); err != nil {
 		t.Fatalf("writePacket: %v", err)
 	}
+
 	// While we read out the packet, a key change will be
 	// initiated.
-	if _, err := trC.readPacket(); err != nil {
-		t.Fatalf("readPacket(client): %v", err)
-	}
+	done := make(chan int, 1)
+	go func() {
+		defer close(done)
+		if _, err := trC.readPacket(); err != nil {
+			t.Fatalf("readPacket(client): %v", err)
+		}
 
+	}()
+
+	<-done
 	<-sync.called
 }
 
