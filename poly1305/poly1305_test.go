d0124b55559e4559aa5e94d2a497ef2dac143c23
Michael Munday <munday@ca.ibm.com>
2017-01-22 11:08:53 -0500

    poly1305: add optimized s390x implementation
    Generic implementation adapted from the amd64 assembly. SIMD
    implementation based the on the algorithm outlined in:
    NEON crypto, Daniel J. Bernstein and Peter Schwabe
    https://cryptojedi.org/papers/neoncrypto-20120320.pdf
    
    The new assembly requires Go 1.8 or above.
    
    Change-Id: Ia72099a600ae843fa443c95e08ec60f743e32369

--- poly1305/poly1305_test.go
+++ poly1305/poly1305_test.go
@@ -75,7 +75,7 @@ var testData = []struct {
 	},
 }
 
-func testSum(t *testing.T, unaligned bool) {
+func testSum(t *testing.T, unaligned bool, sumImpl func(tag *[TagSize]byte, msg []byte, key *[32]byte)) {
 	var out [16]byte
 	var key [32]byte
 
@@ -85,7 +85,7 @@ func testSum(t *testing.T, unaligned bool) {
 			in = unalignBytes(in)
 		}
 		copy(key[:], v.k)
-		Sum(&out, in, &key)
+		sumImpl(&out, in, &key)
 		if !bytes.Equal(out[:], v.correct) {
 			t.Errorf("%d: expected %x, got %x", i, v.correct, out[:])
 		}
@@ -125,8 +125,10 @@ func TestBurnin(t *testing.T) {
 	}
 }
 
-func TestSum(t *testing.T)          { testSum(t, false) }
-func TestSumUnaligned(t *testing.T) { testSum(t, true) }
+func TestSum(t *testing.T)                 { testSum(t, false, Sum) }
+func TestSumUnaligned(t *testing.T)        { testSum(t, true, Sum) }
+func TestSumGeneric(t *testing.T)          { testSum(t, false, sumGeneric) }
+func TestSumGenericUnaligned(t *testing.T) { testSum(t, true, sumGeneric) }
 
 func benchmark(b *testing.B, size int, unaligned bool) {
 	var out [16]byte
