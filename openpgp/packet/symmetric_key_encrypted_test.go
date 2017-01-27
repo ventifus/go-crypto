df26a732bc6efa1a577a6dec25cd9d2d376f2069
mdp <m@mdp.im>
2017-01-26 17:24:59 -0800

    crypto: Fix openpgp Encrypted Session Key for aes192
    The existing implementation checks to see if the session key size is a
    multiple of the chiper blocksize. This fails for AES192 which has a
    keysize of 24 bytes and a 16 byte block size. Instead it should check
    to ensure that the Session Key length is a multiple of the cipher
    blocksize OR if it is equal.
    
    Fixes golang/go#17060
    
    Change-Id: I1dc78129f7fb2ca5ec71b650a2adcb3752dca885

--- openpgp/packet/symmetric_key_encrypted_test.go
+++ openpgp/packet/symmetric_key_encrypted_test.go
@@ -61,43 +61,57 @@ func TestSymmetricKeyEncrypted(t *testing.T) {
 const symmetricallyEncryptedHex = "8c0d04030302371a0b38d884f02060c91cf97c9973b8e58e028e9501708ccfe618fb92afef7fa2d80ddadd93cf"
 const symmetricallyEncryptedContentsHex = "cb1062004d14c4df636f6e74656e74732e0a"
 
-func TestSerializeSymmetricKeyEncrypted(t *testing.T) {
-	buf := bytes.NewBuffer(nil)
-	passphrase := []byte("testing")
-	const cipherFunc = CipherAES128
-	config := &Config{
-		DefaultCipher: cipherFunc,
+func TestSerializeSymmetricKeyEncryptedCiphers(t *testing.T) {
+	var tests = [...]struct {
+		fn   CipherFunction
+		name string
+	}{
+		0: {Cipher3DES, "Cipher3DES"},
+		1: {CipherCAST5, "CipherCAST5"},
+		2: {CipherAES128, "CipherAES128"},
+		3: {CipherAES192, "CipherAES192"},
+		4: {CipherAES256, "CipherAES256"},
 	}
+	for i, tt := range tests {
+		buf := bytes.NewBuffer(nil)
+		passphrase := []byte("testing")
+		var cipherFunc = tt.fn
+		config := &Config{
+			DefaultCipher: cipherFunc,
+		}
 
-	key, err := SerializeSymmetricKeyEncrypted(buf, passphrase, config)
-	if err != nil {
-		t.Errorf("failed to serialize: %s", err)
-		return
-	}
+		key, err := SerializeSymmetricKeyEncrypted(buf, passphrase, config)
+		if err != nil {
+			t.Errorf("#%d cipher(%s) failed to serialize: %s", i, tt.name, err)
+			continue
+		}
 
-	p, err := Read(buf)
-	if err != nil {
-		t.Errorf("failed to reparse: %s", err)
-		return
-	}
-	ske, ok := p.(*SymmetricKeyEncrypted)
-	if !ok {
-		t.Errorf("parsed a different packet type: %#v", p)
-		return
-	}
+		p, err := Read(buf)
+		if err != nil {
+			t.Errorf("#%d cipher(%s) failed to reparse: %s", i, tt.name, err)
+			continue
+		}
 
-	if ske.CipherFunc != config.DefaultCipher {
-		t.Errorf("SKE cipher function is %d (expected %d)", ske.CipherFunc, config.DefaultCipher)
-	}
-	parsedKey, parsedCipherFunc, err := ske.Decrypt(passphrase)
-	if err != nil {
-		t.Errorf("failed to decrypt reparsed SKE: %s", err)
-		return
-	}
-	if !bytes.Equal(key, parsedKey) {
-		t.Errorf("keys don't match after Decrypt: %x (original) vs %x (parsed)", key, parsedKey)
-	}
-	if parsedCipherFunc != cipherFunc {
-		t.Errorf("cipher function doesn't match after Decrypt: %d (original) vs %d (parsed)", cipherFunc, parsedCipherFunc)
+		ske, ok := p.(*SymmetricKeyEncrypted)
+		if !ok {
+			t.Errorf("#%d cipher(%s) parsed a different packet type: %#v", i, tt.name, p)
+			continue
+		}
+
+		if ske.CipherFunc != config.DefaultCipher {
+			t.Errorf("#%d cipher(%s) SKE cipher function is %d (expected %d)", i, tt.name, ske.CipherFunc, config.DefaultCipher)
+		}
+		parsedKey, parsedCipherFunc, err := ske.Decrypt(passphrase)
+		if err != nil {
+			t.Errorf("#%d cipher(%s) failed to decrypt reparsed SKE: %s", i, tt.name, err)
+			continue
+		}
+		if !bytes.Equal(key, parsedKey) {
+			t.Errorf("#%d cipher(%s) keys don't match after Decrypt: %x (original) vs %x (parsed)", i, tt.name, key, parsedKey)
+		}
+		if parsedCipherFunc != cipherFunc {
+			t.Errorf("#%d cipher(%s) cipher function doesn't match after Decrypt: %d (original) vs %d (parsed)",
+				i, tt.name, cipherFunc, parsedCipherFunc)
+		}
 	}
 }
