9ae3566c507155ac4a1d25143bcb7033f61438cd
Spencer Tung <spencertung@google.com>
2017-05-26 11:37:39 -0700

    ssh: return authErr array if all authentication attempts fail
    Change-Id: I4d6cab266410a8c7960073665eddf8935693087f

--- ssh/server.go
+++ ssh/server.go
@@ -272,12 +272,37 @@ func checkSourceAddress(addr net.Addr, sourceAddrs string) error {
 	return fmt.Errorf("ssh: remote address %v is not allowed because of source-address restriction", addr)
 }
 
+// ServerAuthError implements the error interface. It appends any authentication
+// errors that may occur, and is returned if all of the authentication methods
+// provided by the user failed to authenticate.
+type ServerAuthError struct {
+	errMsg string
+
+	// AuthErrors contains an array of authentication errors returned by the
+	// implemented authentication callback method.
+	AuthErrors []error
+}
+
+func (l ServerAuthError) Error() string {
+	return l.errMsg
+}
+
+// NewServerAuthError creates a new ServerAuthError when provided with an error and error
+// array.
+func NewServerAuthError(err error, authErrs []error) *ServerAuthError {
+	return &ServerAuthError{
+		errMsg:     err.Error(),
+		AuthErrors: authErrs,
+	}
+}
+
 func (s *connection) serverAuthenticate(config *ServerConfig) (*Permissions, error) {
 	sessionID := s.transport.getSessionID()
 	var cache pubKeyCache
 	var perms *Permissions
 
 	authFailures := 0
+	var authErrs []error
 
 userAuthLoop:
 	for {
@@ -296,6 +321,9 @@ userAuthLoop:
 
 		var userAuthReq userAuthRequestMsg
 		if packet, err := s.transport.readPacket(); err != nil {
+			if err == io.EOF {
+				return nil, NewServerAuthError(err, authErrs)
+			}
 			return nil, err
 		} else if err = Unmarshal(packet, &userAuthReq); err != nil {
 			return nil, err
@@ -431,6 +459,8 @@ userAuthLoop:
 		default:
 			authErr = fmt.Errorf("ssh: unknown method %q", userAuthReq.Method)
 		}
+
+		authErrs = append(authErrs, authErr)
 
 		if config.AuthLogCallback != nil {
 			config.AuthLogCallback(s, userAuthReq.Method, authErr)
