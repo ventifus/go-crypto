9fa03231623d294f47d2d5cbf82d782e6d294a31
George Tankersley <george.tankersley@gmail.com>
2017-10-18 00:00:00 +0000

    ed25519: improve EdDSA performance by 20-50% on amd64
    This adds an unsaturated 64-bit field implementation (in radix 2^51)
    derived from the amd64-51-30k code in SUPERCOP. In addition to the new
    representation, it uses assembly implementations of field squaring and
    multiplication.
    
    It shows improvement across a wide range of machines:
    
    Intel(R) Xeon(R) CPU E5-2666 v3 @ 2.90GHz (EC2 c4.large):
    
    name             old time/op  new time/op  delta
    KeyGeneration-2  61.2µs ± 0%  35.4µs ± 0%  -42.22%  (p=0.008 n=5+5)
    Signing-2        62.9µs ± 0%  37.0µs ± 0%  -41.18%  (p=0.008 n=5+5)
    Verification-2    163µs ± 0%    85µs ± 0%  -47.83%  (p=0.008 n=5+5)
    
    Intel(R) Celeron(R) CPU N2840 @ 2.16GHz (Acer Chromebook):
    
    name             old time/op  new time/op  delta
    KeyGeneration-2   155µs ± 1%   122µs ± 2%  -21.45%  (p=0.008 n=5+5)
    Signing-2         167µs ± 2%   134µs ± 1%  -20.17%  (p=0.008 n=5+5)
    Verification-2    430µs ± 2%   337µs ± 1%  -21.47%  (p=0.008 n=5+5)
    
    Change-Id: I839e082fc1f87d0b8275df8f0b4fced427786f1c

--- ed25519/internal/edwards25519/const.go
+++ ed25519/internal/edwards25519/const.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !amd64
+
 package edwards25519
 
 // These values are from the public domain, “ref10” implementation of ed25519
