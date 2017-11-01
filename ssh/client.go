2df25f72d36d1ff0e17a7c7d4c26861b253ebc79
Greyh4t <greyh4t1337@gmail.com>
2017-11-01 22:35:09 +0800

    ssh: fix invalidation of the timeout when target is not a ssh server
    When the service on port 22 is redis, the dial function will hang on
    read version, the timeout will be invalid.
    
    Change-Id: I8fd6cbc62272f553fa571e0196c069a5ca05efca

--- ssh/client.go
+++ ssh/client.go
@@ -173,7 +173,11 @@ func Dial(network, addr string, config *ClientConfig) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	if config.Timeout > 0 {
+		conn.SetReadDeadline(time.Now().Add(config.Timeout))
+	}
 	c, chans, reqs, err := NewClientConn(conn, addr, config)
+	conn.SetReadDeadline(time.Time{})
 	if err != nil {
 		return nil, err
 	}
