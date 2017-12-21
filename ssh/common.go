8e806aa8e35683d09f8d82c3b5a581928fee7648
Peter Sanford <psanford@sanford.io>
2017-12-21 07:57:42 -0800

    ssh: remove arcfour{128,256} from default cipher list
    Following in OpenSSH's footsteps, remove arcfour{128,256} from the
    default cipher list. These can still be enabled by setting them in
    Config.Ciphers.
    
    Fixes golang/go#23173
    
    Change-Id: I9301dbfc8b3550cacb718883387c1ba2c1d777ad

--- ssh/common.go
+++ ssh/common.go
@@ -28,7 +28,6 @@ const (
 var supportedCiphers = []string{
 	"aes128-ctr", "aes192-ctr", "aes256-ctr",
 	"aes128-gcm@openssh.com",
-	"arcfour256", "arcfour128",
 }
 
 // supportedKexAlgos specifies the supported key-exchange algorithms in
