c2ee34d71b2ab5a4cb4f75733cf2e9d2c18bceaa
Jonathan Turner <jt@jtnet.co.uk>
2017-12-27 19:23:43 +0000

    pbkdf2: use larger int64 iteration counts and key lengths
    The function Key64 has been added to allow larger iteration counts and
    key lengths by using the int64 type rather than int for these values.
    This is useful to enable iteration counts larger than 4294967295 on a
    32bit system with this new function. An example of where this is required
    is https://tools.ietf.org/html/rfc3962 section 4. An iteration count of
    4294967296 is defined which cannot be fulfilled with int on a 32bit system.
    The pre-existing Key function has been updated to use the Key64 function
    to minimise code duplication and ensure backwards compatibility.
    
    Change-Id: I78fffed50d8e76c25cbb924a4b85b8e89948c7c2

--- pbkdf2/pbkdf2.go
+++ pbkdf2/pbkdf2.go
@@ -40,14 +40,35 @@ import (
 // Using a higher iteration count will increase the cost of an exhaustive
 // search but will also make derivation proportionally slower.
 func Key(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
+	return Key64(password, salt, int64(iter), int64(keyLen), h)
+}
+
+// Key64 derives a key from the password, salt and iteration count, returning a
+// []byte of length keylen that can be used as cryptographic key. Key64 uses
+// int64 for the iteration count and key length to allow larger values.
+// The key is derived based on the method described as PBKDF2 with the HMAC
+// variant using the supplied hash function.
+//
+// For example, to use a HMAC-SHA-1 based PBKDF2 key derivation function, you
+// can get a derived key for e.g. AES-256 (which needs a 32-byte key) by
+// doing:
+//
+// 	dk := pbkdf2.Key([]byte("some password"), salt, 4096, 32, sha1.New)
+//
+// Remember to get a good random salt. At least 8 bytes is recommended by the
+// RFC.
+//
+// Using a higher iteration count will increase the cost of an exhaustive
+// search but will also make derivation proportionally slower.
+func Key64(password, salt []byte, iter, keyLen int64, h func() hash.Hash) []byte {
 	prf := hmac.New(h, password)
-	hashLen := prf.Size()
+	hashLen := int64(prf.Size())
 	numBlocks := (keyLen + hashLen - 1) / hashLen
 
 	var buf [4]byte
 	dk := make([]byte, 0, numBlocks*hashLen)
 	U := make([]byte, hashLen)
-	for block := 1; block <= numBlocks; block++ {
+	for block := int64(1); block <= numBlocks; block++ {
 		// N.B.: || means concatenation, ^ means XOR
 		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
 		// U_1 = PRF(password, salt || uint(i))
@@ -59,11 +80,11 @@ func Key(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
 		buf[3] = byte(block)
 		prf.Write(buf[:4])
 		dk = prf.Sum(dk)
-		T := dk[len(dk)-hashLen:]
+		T := dk[int64(len(dk))-hashLen:]
 		copy(U, T)
 
 		// U_n = PRF(password, U_(n-1))
-		for n := 2; n <= iter; n++ {
+		for n := int64(2); n <= iter; n++ {
 			prf.Reset()
 			prf.Write(U)
 			U = U[:0]
