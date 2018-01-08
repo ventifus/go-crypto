0cde2ad6ae6d1fe410e2dd63071c29499f662050
Justin Gracenin <jgracenin@gmail.com>
2018-01-08 14:34:49 +0000

    ssh: fix protocol version exchange (for multi-line)
    Fixes golang/go#23194
    
    During SSH Protocol Version Exchange, a client may send metadata lines
    prior to sending the SSH version string. To conform to the RFC, all SSH
    implementations must support this (minimally, clients can ignore the
    metadata lines).
    
    For example, this is valid:
    some-metadata
    SSH-2.0-OpenSSH
    
    The current Go implementation takes the first line it sees as
    the version string (in this case, some-metadata). Then, it uses
    the next line (SSH-2.0-OpenSSH) as part of key exchange, which
    is guaranteed to fail.
    
    Unfortunately, this SSH feature is used by some vendors and is part
    of the official RFC: https://tools.ietf.org/html/rfc4253#section-4.2
    
    Change-Id: I7be61700a07756353875bf43aad09a580ba533ff

--- ssh/transport.go
+++ ssh/transport.go
@@ -6,6 +6,7 @@ package ssh
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"io"
 	"log"
@@ -319,7 +320,7 @@ func exchangeVersions(rw io.ReadWriter, versionLine []byte) (them []byte, err er
 	for _, c := range versionLine {
 		// The spec disallows non US-ASCII chars, and
 		// specifically forbids null chars.
-		if c < 32 {
+		if c < 32 && c != '\r' && c != '\n' {
 			return nil, errors.New("ssh: junk character in version line")
 		}
 	}
@@ -342,7 +343,7 @@ func readVersion(r io.Reader) ([]byte, error) {
 	var ok bool
 	var buf [1]byte
 
-	for len(versionString) < maxVersionStringBytes {
+	for length := 0; length < maxVersionStringBytes; length++ {
 		_, err := io.ReadFull(r, buf[:])
 		if err != nil {
 			return nil, err
@@ -350,6 +351,13 @@ func readVersion(r io.Reader) ([]byte, error) {
 		// The RFC says that the version should be terminated with \r\n
 		// but several SSH servers actually only send a \n.
 		if buf[0] == '\n' {
+			if !bytes.HasPrefix(versionString, []byte("SSH-")) {
+				// RFC 4253 says we need to ignore all version string lines
+				// except the one containing the SSH version (provided that
+				// all the lines do not exceed 255 bytes in total).
+				versionString = make([]byte, 0, 64)
+				continue
+			}
 			ok = true
 			break
 		}
