bbab2e248be5ffd6a72a3b58b0e747b355b42e0c
Filippo Valsorda <filippo@golang.org>
2018-03-15 16:20:44 -0400

    openpgp/packet: improve handling of short MPIs for RSA values
    MPIs are (supposed to be) stripped of leading zeroes. Avoid passing
    such short values into crypto/rsa, even if it currently happens to work.
    
    Also fix an error handling bug in EncryptedKey parsing.
    
    Change-Id: I5a5f4813b8358e83fcc2deeda1272d2733814542

--- openpgp/packet/encrypted_key.go
+++ openpgp/packet/encrypted_key.go
@@ -49,6 +49,9 @@ func (e *EncryptedKey) parse(r io.Reader) (err error) {
 		}
 		e.encryptedMPI2.bytes, e.encryptedMPI2.bitLength, err = readMPI(r)
 	}
+	if err != nil {
+		return
+	}
 	_, err = consumeAll(r)
 	return
 }
@@ -72,7 +75,8 @@ func (e *EncryptedKey) Decrypt(priv *PrivateKey, config *Config) error {
 	// padding oracle attacks.
 	switch priv.PubKeyAlgo {
 	case PubKeyAlgoRSA, PubKeyAlgoRSAEncryptOnly:
-		b, err = rsa.DecryptPKCS1v15(config.Random(), priv.PrivateKey.(*rsa.PrivateKey), e.encryptedMPI1.bytes)
+		k := priv.PrivateKey.(*rsa.PrivateKey)
+		b, err = rsa.DecryptPKCS1v15(config.Random(), k, padToKeySize(&k.PublicKey, e.encryptedMPI1.bytes))
 	case PubKeyAlgoElGamal:
 		c1 := new(big.Int).SetBytes(e.encryptedMPI1.bytes)
 		c2 := new(big.Int).SetBytes(e.encryptedMPI2.bytes)
