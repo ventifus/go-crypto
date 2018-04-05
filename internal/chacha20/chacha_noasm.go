fd64e3ffd18c80c1870977eb5a900b73ec42ec76
Paulo Flabiano Smorigo <pfsmorigo@linux.vnet.ibm.com>
2018-04-05 14:19:48 +0000

    internal/chacha20: improve performance for ppc64le
    Add asm implementation for chacha20 using vector instructions on ppc64le.
    Below, the difference using the new code:
    
                                 old MB/s       new MB/s      speedup
    BenchmarkChaCha20/32-3       133.21 MB/s    97.73 MB/s     0.73x
    BenchmarkChaCha20/63-3       240.29 MB/s    170.88 MB/s    0.71x
    BenchmarkChaCha20/64-3       280.50 MB/s    173.81 MB/s    0.61x
    BenchmarkChaCha20/256-3      311.90 MB/s    975.11 MB/s    3.12x
    BenchmarkChaCha20/1024-3     332.25 MB/s    1127.60 MB/s   3.39x
    BenchmarkChaCha20/1350-3     320.24 MB/s    921.70 MB/s    2.87x
    BenchmarkChaCha20/65536-3    335.89 MB/s    1193.86 MB/s   3.55x
    
    Small sizes are slow due the fact that it always calculates using
    256 bytes of key stream.
    
    Fixes golang/go#25051
    
    Change-Id: I0f95ba8d10a486be98ac94a97ede9f089591dbdf

--- internal/chacha20/chacha_noasm.go
+++ internal/chacha20/chacha_noasm.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !s390x gccgo appengine
+// +build !s390x,!ppc64le gccgo appengine
 
 package chacha20
 
