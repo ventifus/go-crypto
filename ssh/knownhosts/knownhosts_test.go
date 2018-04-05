59daa422e7eb000e9a0c2078210d306043803294
Han-Wen Nienhuys <hanwen@google.com>
2018-04-05 16:16:06 +0200

    ssh/knownhosts: disregard IP address if the hostname is available
    This fixes the following vulnerability scenario:
    
    * Victim logs into SAFE-HOST on SAFE-IP-ADDRESS regularly.
    
    * Victim is cajoled into connecting to attacker controlled
      ATTACK-HOST, on ATTACK-IP-ADDRESS. ATTACK-HOST uses a different host
      key type (e.g. Ed25519 vs RSA). The new key is added at the end of
      known_hosts.
    
    * Attacker makes DNS system return ATTACK-IP-ADDRESS for SAFE-HOST.
    
    * Victim logs into SAFE-HOST, but is not warned because the host key
      matches ATTACK-IP-ADDRESS.
    
    For this attack to work, the key type has to be different, because
    knownhosts gives precedence to the first key found for each type. Add
    a test that asserts this behavior.
    
    The new semantics simplify the code, but callers that modify
    .ssh/known_host interactviely must now take an extra step to remain
    OpenSSH compatible: on successful login, the IP address must be
    checked without hostname, and if it is not known, added separately to
    the known_hosts file, so future logins that use an IP address only
    will be protected too.
    
    Change-Id: I62b1b60ceb02e2f583a4657213feac1a8885dd42

--- ssh/knownhosts/knownhosts_test.go
+++ ssh/knownhosts/knownhosts_test.go
@@ -7,6 +7,7 @@ package knownhosts
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"net"
 	"reflect"
 	"testing"
@@ -166,7 +167,7 @@ func TestBasic(t *testing.T) {
 	str := fmt.Sprintf("#comment\n\nserver.org,%s %s\notherhost %s", testAddr, edKeyStr, ecKeyStr)
 	db := testDB(t, str)
 	if err := db.check("server.org:22", testAddr, edKey); err != nil {
-		t.Errorf("got error %q, want none", err)
+		t.Errorf("got error %v, want none", err)
 	}
 
 	want := KnownKey{
@@ -183,6 +184,36 @@ func TestBasic(t *testing.T) {
 	} else if !reflect.DeepEqual(ke.Want[0], want) {
 		t.Errorf("got %v, want %v", ke.Want[0], want)
 	}
+}
+
+func TestHostNamePrecedence(t *testing.T) {
+	var evilAddr = &net.TCPAddr{
+		IP:   net.IP{66, 66, 66, 66},
+		Port: 22,
+	}
+
+	str := fmt.Sprintf("server.org,%s %s\nevil.org,%s %s", testAddr, edKeyStr, evilAddr, ecKeyStr)
+	db := testDB(t, str)
+
+	if err := db.check("server.org:22", evilAddr, ecKey); err == nil {
+		t.Errorf("succeeded, want KeyError")
+	} else if _, ok := err.(*KeyError); !ok {
+		t.Errorf("got %T, want *KeyError", err)
+	} else {
+		log.Println(err)
+	}
+}
+
+func TestDBOrderingPrecedenceKeyType(t *testing.T) {
+	str := fmt.Sprintf("server.org,%s %s\nserver.org,%s %s", testAddr, edKeyStr, testAddr, alternateEdKeyStr)
+	db := testDB(t, str)
+
+	if err := db.check("server.org:22", testAddr, alternateEdKey); err == nil {
+		t.Errorf("succeeded, want KeyError")
+	} else if _, ok := err.(*KeyError); !ok {
+		t.Errorf("got %T, want *KeyError", err)
+	}
+>>>>>>> 54cff0c... ssh/knownhosts: disregard IP address if the hostname is available
 }
 
 func TestNegate(t *testing.T) {
