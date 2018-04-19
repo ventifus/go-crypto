4235dc8f08eceb4cc6b079796a75b00882fad4a3
Meng Zhuo <mengzhuo1203@gmail.com>
2018-04-19 17:51:40 +0800

    internal/chacha20: add arm64 SIMD implementation
    Inspired by Vectorization of ChaCha Stream Cipher
    https://eprint.iacr.org/2013/759.pdf
    
    name                       old time/op    new time/op     delta
    Chacha20Poly1305Open_64      2.46µs ± 0%     2.46µs ± 0%      ~     (p=0.304 n=10+10)
    Chacha20Poly1305Seal_64      3.39µs ± 0%     2.26µs ± 1%   -33.53%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_1350    36.1µs ± 0%     13.4µs ± 0%   -63.04%  (p=0.000 n=8+10)
    Chacha20Poly1305Seal_1350    25.9µs ± 0%     13.3µs ± 0%   -48.64%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_8K       147µs ± 0%       65µs ± 0%   -55.74%  (p=0.000 n=10+10)
    Chacha20Poly1305Seal_8K       146µs ± 0%       65µs ± 0%   -55.77%  (p=0.000 n=9+10)
    
    name                       old speed      new speed       delta
    Chacha20Poly1305Open_64    26.0MB/s ± 0%   26.0MB/s ± 0%      ~     (p=0.270 n=10+10)
    Chacha20Poly1305Seal_64    18.9MB/s ± 0%   28.4MB/s ± 1%   +50.43%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_1350  37.4MB/s ± 0%  101.1MB/s ± 0%  +170.54%  (p=0.000 n=8+10)
    Chacha20Poly1305Seal_1350  52.1MB/s ± 0%  101.5MB/s ± 0%   +94.70%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_8K    55.9MB/s ± 0%  126.3MB/s ± 0%  +125.91%  (p=0.000 n=10+10)
    Chacha20Poly1305Seal_8K    55.9MB/s ± 0%  126.4MB/s ± 0%  +126.07%  (p=0.000 n=9+10)
    
    Change-Id: I487487faa2ae4ff29de6fd8eb1317740c2939c10

---
+++ internal/chacha20/chacha_arm64.go
@@ -0,0 +1,65 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package chacha20
+
+import (
+	"encoding/binary"
+)
+
+const blockSize = 64
+
+func XORKeyStream(out, in []byte, counter *[16]byte, key *[32]byte) {
+
+	var state [16]uint32
+	state[0] = 0x61707865
+	state[1] = 0x3320646e
+	state[2] = 0x79622d32
+	state[3] = 0x6b206574
+	state[4] = binary.LittleEndian.Uint32(key[0:4])
+	state[5] = binary.LittleEndian.Uint32(key[4:8])
+	state[6] = binary.LittleEndian.Uint32(key[8:12])
+	state[7] = binary.LittleEndian.Uint32(key[12:16])
+	state[8] = binary.LittleEndian.Uint32(key[16:20])
+	state[9] = binary.LittleEndian.Uint32(key[20:24])
+	state[10] = binary.LittleEndian.Uint32(key[24:28])
+	state[11] = binary.LittleEndian.Uint32(key[28:32])
+	state[12] = binary.LittleEndian.Uint32(counter[0:4])
+	state[13] = binary.LittleEndian.Uint32(counter[4:8])
+	state[14] = binary.LittleEndian.Uint32(counter[8:12])
+	state[15] = binary.LittleEndian.Uint32(counter[12:16])
+
+	for len(in) >= blockSize*4 {
+		block4(&state, out, in)
+		in = in[blockSize*4:]
+		out = out[blockSize*4:]
+		state[12] += 4
+	}
+
+	if len(in) == 0 {
+		return
+	}
+
+	for len(in) >= blockSize {
+		block(&state, out, in)
+		in = in[blockSize:]
+		out = out[blockSize:]
+		state[12] += 1
+	}
+
+	if len(in) == 0 {
+		return
+	}
+
+	buf := make([]byte, blockSize, blockSize)
+	copy(buf, in)
+	block(&state, buf, buf)
+	copy(out, buf)
+}
+
+//go:noescape
+func block(state *[16]uint32, out, in []byte)
+
+//go:noescape
+func block4(state *[16]uint32, out, in []byte)
