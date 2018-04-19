a1fcbd8abfa01d0d2e6b2e62d42d85fae3e3fdf2
Meng Zhuo <mengzhuo1203@gmail.com>
2018-04-19 19:22:33 +0800

    internal/chacha20: add arm64 SIMD implementation
    Inspired by Vectorization of ChaCha Stream Cipher
    https://eprint.iacr.org/2013/759.pdf
    
    name                       old time/op    new time/op     delta
    Chacha20Poly1305Open_64      2.46µs ± 0%     2.46µs ± 0%      ~     (p=0.304 n=10+10)
    Chacha20Poly1305Seal_64      3.39µs ± 0%     2.26µs ± 1%   -33.53%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_1350    36.1µs ± 0%     13.4µs ± 0%   -63.04%  (p=0.000 n=8+10)
    Chacha20Poly1305Seal_1350    25.9µs ± 0%     13.3µs ± 0%   -48.64%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_8K       147µs ± 0%       65µs ± 0%   -55.74%  (p=0.000 n=10+10)
    Chacha20Poly1305Seal_8K       146µs ± 0%       65µs ± 0%   -55.77%  (p=0.000 n=9+10)
    
    name                       old speed      new speed       delta
    Chacha20Poly1305Open_64    26.0MB/s ± 0%   26.0MB/s ± 0%      ~     (p=0.270 n=10+10)
    Chacha20Poly1305Seal_64    18.9MB/s ± 0%   28.4MB/s ± 1%   +50.43%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_1350  37.4MB/s ± 0%  101.1MB/s ± 0%  +170.54%  (p=0.000 n=8+10)
    Chacha20Poly1305Seal_1350  52.1MB/s ± 0%  101.5MB/s ± 0%   +94.70%  (p=0.000 n=9+10)
    Chacha20Poly1305Open_8K    55.9MB/s ± 0%  126.3MB/s ± 0%  +125.91%  (p=0.000 n=10+10)
    Chacha20Poly1305Seal_8K    55.9MB/s ± 0%  126.4MB/s ± 0%  +126.07%  (p=0.000 n=9+10)
    
    Change-Id: I487487faa2ae4ff29de6fd8eb1317740c2939c10

--- internal/chacha20/chacha_generic.go
+++ internal/chacha20/chacha_generic.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !go1.11,arm64
+
 // Package ChaCha20 implements the core ChaCha20 function as specified in https://tools.ietf.org/html/rfc7539#section-2.3.
 package chacha20
 
