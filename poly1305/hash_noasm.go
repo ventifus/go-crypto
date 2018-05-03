1671f24eb3bdcd30e8add63cccbe4d6e701063e3
Andreas Auernhammer <aead@mail.de>
2018-05-04 00:15:25 +0200

    poly1305: implement a subset of the hash.Hash interface
    This CL adds the poly1305.Hash type which implements a
    subset of hash.Hash interface. With Hash it is possible
    to compute an authentication tag of data without copying
    it into a single byte slice.
    
    This commit modifies the reference/generic and the
    AMD64 assembler but not the ARM/s390x implementation
    to support an io.Writer interface.
    
    Updates golang/go#25219
    
    Change-Id: I7ee5a9eadd43387cf3cd887d734c625575eee47d

---
+++ poly1305/hash_noasm.go
@@ -0,0 +1,11 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !amd64 gccgo appengine
+
+package poly1305
+
+type hash hashGeneric
+
+func newHash(key *[32]byte) hash { return hashGeneric(key) }
