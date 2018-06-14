e79f46ba21e4f30034bb818a6eb59097a3cb25b9
lukechampine <luke.champine@gmail.com>
2018-06-14 00:21:52 +0000

    xts: only allocate tweak once
    The call to `k2.Encrypt` causes tweak to escape to the heap, resulting in a 16-byte allocation for each call to `Encrypt`/`Decrypt`. Moving tweak into the `Cipher` struct allows it to be reused, although the array must be cleared before use.
    
    Change-Id: I5e0dd8c2e1a1078a151bbeb1d0760936b6b56216
    GitHub-Last-Rev: 3e18cd75b24ed27d81d592991283e91460861e42
    GitHub-Pull-Request: golang/crypto#51

--- xts/xts.go
+++ xts/xts.go
@@ -25,6 +25,7 @@ import (
 	"crypto/cipher"
 	"encoding/binary"
 	"errors"
+	"sync"
 )
 
 // Cipher contains an expanded key structure. It doesn't contain mutable state
@@ -36,6 +37,12 @@ type Cipher struct {
 // blockSize is the block size that the underlying cipher must have. XTS is
 // only defined for 16-byte ciphers.
 const blockSize = 16
+
+var tweakPool = sync.Pool{
+	New: func() interface{} {
+		return new([blockSize]byte)
+	},
+}
 
 // NewCipher creates a Cipher given a function for creating the underlying
 // block cipher (which must have a block size of 16 bytes). The key must be
@@ -65,7 +72,10 @@ func (c *Cipher) Encrypt(ciphertext, plaintext []byte, sectorNum uint64) {
 		panic("xts: plaintext is not a multiple of the block size")
 	}
 
-	var tweak [blockSize]byte
+	tweak := tweakPool.Get().(*[blockSize]byte)
+	for i := range tweak {
+		tweak[i] = 0
+	}
 	binary.LittleEndian.PutUint64(tweak[:8], sectorNum)
 
 	c.k2.Encrypt(tweak[:], tweak[:])
@@ -81,8 +91,10 @@ func (c *Cipher) Encrypt(ciphertext, plaintext []byte, sectorNum uint64) {
 		plaintext = plaintext[blockSize:]
 		ciphertext = ciphertext[blockSize:]
 
-		mul2(&tweak)
+		mul2(tweak)
 	}
+
+	tweakPool.Put(tweak)
 }
 
 // Decrypt decrypts a sector of ciphertext and puts the result into plaintext.
@@ -96,7 +108,10 @@ func (c *Cipher) Decrypt(plaintext, ciphertext []byte, sectorNum uint64) {
 		panic("xts: ciphertext is not a multiple of the block size")
 	}
 
-	var tweak [blockSize]byte
+	tweak := tweakPool.Get().(*[blockSize]byte)
+	for i := range tweak {
+		tweak[i] = 0
+	}
 	binary.LittleEndian.PutUint64(tweak[:8], sectorNum)
 
 	c.k2.Encrypt(tweak[:], tweak[:])
@@ -112,8 +127,10 @@ func (c *Cipher) Decrypt(plaintext, ciphertext []byte, sectorNum uint64) {
 		plaintext = plaintext[blockSize:]
 		ciphertext = ciphertext[blockSize:]
 
-		mul2(&tweak)
+		mul2(tweak)
 	}
+
+	tweakPool.Put(tweak)
 }
 
 // mul2 multiplies tweak by 2 in GF(2¹²⁸) with an irreducible polynomial of
