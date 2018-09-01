73b6936b7fe2e100cbe03a2205d8a47189487bb2
Paul M Furley <paul@paulfurley.com>
2018-09-01 08:24:41 +0000

    openpgp: Define s2k magic numbers as consts
    Hi folks,
    
    My first contribution, and I'm new to Golang, so please be gentle!
    
    This PR factors out some magic numbers from the openpgp string-to-key code.
    
    Please see the long descriptions in the individual commits.
    
    These commits were some groundwork I laid for implementing encrypted private keys (serializing with a password). I'm submitting this as an initial (hopefully uncontroversial) pull request to get the feel for how you folks like to receive contributions.
    
    Thanks for building crypto/openpgp ... it's been good to hack on, and I've got more contributions lined up for the future!
    
    Change-Id: Ic0fd016b1c39d4873c929a2b72e99ca8c117bf76
    GitHub-Last-Rev: ea089878879aa987ff7d5dab0738fda03ad8cbb2
    GitHub-Pull-Request: golang/crypto#55

--- openpgp/s2k/s2k.go
+++ openpgp/s2k/s2k.go
@@ -15,6 +15,25 @@ import (
 	"golang.org/x/crypto/openpgp/errors"
 )
 
+type stringToKeySpecifier = uint8
+
+const (
+	S2KCountMin     = 1024
+	S2KCountDefault = 65536
+	S2KCountMax     = 65011712
+)
+
+const (
+	// https://tools.ietf.org/html/rfc4880#section-3.7.1.1
+	SimpleS2K stringToKeySpecifier = 0
+
+	// https://tools.ietf.org/html/rfc4880#section-3.7.1.2
+	SaltedS2K stringToKeySpecifier = 1
+
+	// https://tools.ietf.org/html/rfc4880#section-3.7.1.3
+	IteratedAndSaltedS2K stringToKeySpecifier = 3
+)
+
 // Config collects configuration parameters for s2k key-stretching
 // transformatioms. A nil *Config is valid and results in all default
 // values. Currently, Config is used only by the Serialize function in
@@ -46,17 +65,20 @@ func (c *Config) hash() crypto.Hash {
 }
 
 func (c *Config) encodedCount() uint8 {
+	var i int
+
 	if c == nil || c.S2KCount == 0 {
-		return 96 // The common case. Correspoding to 65536
+		i = S2KCountDefault
+	} else {
+		i = c.S2KCount
 	}
 
-	i := c.S2KCount
 	switch {
 	// Behave like GPG. Should we make 65536 the lowest value used?
-	case i < 1024:
-		i = 1024
-	case i > 65011712:
-		i = 65011712
+	case i < S2KCountMin:
+		i = S2KCountMin
+	case i > S2KCountMax:
+		i = S2KCountMax
 	}
 
 	return encodeCount(i)
@@ -68,7 +90,7 @@ func (c *Config) encodedCount() uint8 {
 // if i is not in the above range (encodedCount above takes care to
 // pass i in the correct range). See RFC 4880 Section 3.7.7.1.
 func encodeCount(i int) uint8 {
-	if i < 1024 || i > 65011712 {
+	if i < S2KCountMin || i > S2KCountMax {
 		panic("count arg i outside the required range")
 	}
 
@@ -171,12 +193,12 @@ func Parse(r io.Reader) (f func(out, in []byte), err error) {
 	h := hash.New()
 
 	switch buf[0] {
-	case 0:
+	case SimpleS2K:
 		f := func(out, in []byte) {
 			Simple(out, h, in)
 		}
 		return f, nil
-	case 1:
+	case SaltedS2K:
 		_, err = io.ReadFull(r, buf[:8])
 		if err != nil {
 			return
@@ -185,7 +207,7 @@ func Parse(r io.Reader) (f func(out, in []byte), err error) {
 			Salted(out, h, in, buf[:8])
 		}
 		return f, nil
-	case 3:
+	case IteratedAndSaltedS2K:
 		_, err = io.ReadFull(r, buf[:9])
 		if err != nil {
 			return
@@ -206,7 +228,7 @@ func Parse(r io.Reader) (f func(out, in []byte), err error) {
 // nil. In that case, sensible defaults will be used.
 func Serialize(w io.Writer, key []byte, rand io.Reader, passphrase []byte, c *Config) error {
 	var buf [11]byte
-	buf[0] = 3 /* iterated and salted */
+	buf[0] = IteratedAndSaltedS2K
 	buf[1], _ = HashToHashId(c.hash())
 	salt := buf[2:10]
 	if _, err := io.ReadFull(rand, salt); err != nil {
