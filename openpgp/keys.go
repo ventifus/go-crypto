db2667715cbb5c679643631456791449b05ad378
aviau <alexandre@alexandreviau.net>
2018-09-11 23:30:20 -0400

    openpgp: implement Cert Revocation Signature
    The existing code would ignore user ids that are not immediately
    followed by a self-signature. However, according to RFC4880 11.1,
    they are valid:
    > Immediately following each User ID packet, there are zero or more
    > Signature packets.
    
    This had the effect of also ignoring revoked UserIDs, depending on the
    ordering of the packets. Consider the following packet ordering scenarios:
     A: UID SELFSIG REV
     B: UID REV SELFSIG
    
    In scenario A, the UID would be added to the Entity's Identities, but in
    Scenario B, it wouldn't.
    
    Bring consistenty by adding UIDs to the entity's identities whether or
    not they are followed by a signature.
    
    Implement Certification revocation signature as defined by
    RFC4880 5.2.1.
    
    Create a new Revocations slice in Identity and add Certification
    revocation signatures in that slice.
    
    Fixes golang/go#27602
    
    Change-Id: Ia29914fbab5e49ae72b1851ea1b75e38245fec27

--- openpgp/keys.go
+++ openpgp/keys.go
@@ -38,6 +38,7 @@ type Identity struct {
 	UserId        *packet.UserId
 	SelfSignature *packet.Signature
 	Signatures    []*packet.Signature
+	Revocations   []*packet.Signature
 }
 
 // A Subkey is an additional public key in an Entity. Subkeys can be used for
@@ -399,9 +400,6 @@ EachPacket:
 }
 
 func addUserID(e *Entity, packets *packet.Reader, pkt *packet.UserId) error {
-	// Make a new Identity object, that we might wind up throwing away.
-	// We'll only add it if we get a valid self-signature over this
-	// userID.
 	identity := new(Identity)
 	identity.Name = pkt.Id
 	identity.UserId = pkt
@@ -420,16 +418,26 @@ func addUserID(e *Entity, packets *packet.Reader, pkt *packet.UserId) error {
 			break
 		}
 
-		if (sig.SigType == packet.SigTypePositiveCert || sig.SigType == packet.SigTypeGenericCert) && sig.IssuerKeyId != nil && *sig.IssuerKeyId == e.PrimaryKey.KeyId {
-			if err = e.PrimaryKey.VerifyUserIdSignature(pkt.Id, e.PrimaryKey, sig); err != nil {
-				return errors.StructuralError("user ID self-signature invalid: " + err.Error())
+		switch sig.SigType {
+		case packet.SigTypeCertRevocation:
+			identity.Revocations = append(identity.Revocations, sig)
+		case packet.SigTypePositiveCert, packet.SigTypeGenericCert:
+			if sig.IssuerKeyId != nil && *sig.IssuerKeyId == e.PrimaryKey.KeyId {
+				if err = e.PrimaryKey.VerifyUserIdSignature(pkt.Id, e.PrimaryKey, sig); err != nil {
+					return errors.StructuralError("user ID self-signature invalid: " + err.Error())
+				}
+				if identity.SelfSignature == nil {
+					identity.SelfSignature = sig
+				}
+				break
 			}
-			identity.SelfSignature = sig
-			e.Identities[pkt.Id] = identity
-		} else {
+			fallthrough // It isn't a self-sig, treat it as a normal signature
+		default:
 			identity.Signatures = append(identity.Signatures, sig)
 		}
 	}
+
+	e.Identities[identity.Name] = identity
 
 	return nil
 }
