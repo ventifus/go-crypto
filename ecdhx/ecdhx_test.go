5e40c67d6a0beaf985cbad83f43bbedd1b7bd46d
armfazh <armfazh@cloudflare.com>
2018-12-21 18:08:04 -0800

    x/crypto: Adds Fp25519/448 and X25519/X448 for amd64 arch
    The existing package x/crypto/curve25519 only provides
    the implementation of X25519. This change provides X448 and
    efficient field arithmetic for GF(2^255-19) and GF(2^448-2^224-1).
    Contains optimized code for amd64 architectures including
    pre- and post-Haswell variants.
    
    It follows the implementation of reported in http://ia.cr/2017/264
    
    Fixes golang/go#29390
    
    Change-Id: I62c5d5e10f948f28f1c9a202ef566be5a67189c7

---
+++ ecdhx/ecdhx_test.go
@@ -0,0 +1,68 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build amd64
+
+package ecdhx_test
+
+import (
+	"fmt"
+	"testing"
+
+	dh "golang.org/x/crypto/ecdhx"
+)
+
+func baseTest(t *testing.T, x, base dh.XKey) {
+	const times = 1 << 13
+	y := x
+	for i := 0; i < times; i++ {
+		want, got := x.Shared(base), y.KeyGen()
+		x, y = want, got
+		if got != want {
+			t.Errorf("[incorrect result]\ninput: %v\ngot:   %v\nwant:  %v\n", x, got, want)
+		}
+	}
+}
+
+func TestBase(t *testing.T) {
+	t.Run("X25519", func(t *testing.T) { baseTest(t, dh.RandomKey255(), dh.GetBase255()) })
+	t.Run("X448", func(t *testing.T) { baseTest(t, dh.RandomKey448(), dh.GetBase448()) })
+}
+
+func ecdhx(b *testing.B, x, y dh.XKey) {
+	b.SetBytes(int64(x.Size()))
+	b.Run("KeyGen", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			x = x.KeyGen()
+		}
+	})
+	b.Run("Shared", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			z := x.Shared(y)
+			y = x
+			x = z
+		}
+	})
+}
+
+func BenchmarkECDHX(b *testing.B) {
+	b.Run("X25519", func(b *testing.B) { ecdhx(b, dh.RandomKey255(), dh.RandomKey255()) })
+	b.Run("X448", func(b *testing.B) { ecdhx(b, dh.RandomKey448(), dh.RandomKey448()) })
+}
+
+func Example_x25519() {
+	// Generating Alice's secret and public keys
+	aliceSecret := dh.RandomKey255()
+	alicePublic := aliceSecret.KeyGen()
+	// Generating Bob's secret and public keys
+	bobSecret := dh.RandomKey255()
+	bobPublic := bobSecret.KeyGen()
+	// Deriving Alice's shared key
+	aliceShared := aliceSecret.Shared(bobPublic)
+	// Deriving Bob's shared key
+	bobShared := bobSecret.Shared(alicePublic)
+
+	fmt.Println(aliceShared == bobShared)
+	// Output: true
+}
---
+++ ecdhx/wycheproof_test.go
@@ -0,0 +1,70 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build amd64
+
+package ecdhx_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	dh "golang.org/x/crypto/ecdhx"
+)
+
+type WycheproofTestVector struct {
+	TcId    int
+	Public  dh.XKey
+	Private dh.XKey
+	Shared  dh.XKey
+}
+
+func readVectors(t *testing.T) []WycheproofTestVector {
+	type WycheproofRaw struct {
+		TcId    int
+		Comment string
+		Curve   string
+		Public  string
+		Private string
+		Shared  string
+		Result  string
+		Flags   []string
+	}
+	nameFile := "testdata/x25519_test.json"
+	jsonFile, err := os.Open(nameFile)
+	if err != nil {
+		t.Fatalf("File %v can not be opened. Error: %v", nameFile, err)
+	}
+	defer jsonFile.Close()
+
+	input, _ := ioutil.ReadAll(jsonFile)
+	var vectorsRaw []WycheproofRaw
+
+	err = json.Unmarshal(input, &vectorsRaw)
+	if err != nil {
+		t.Fatalf("File %v can not be loaded. Error: %v", nameFile, err)
+	}
+	vectors := make([]WycheproofTestVector, len(vectorsRaw))
+	for i, v := range vectorsRaw {
+		vectors[i].TcId = v.TcId
+		vectors[i].Public = stringToKey(v.Public, dh.SizeKey255)
+		vectors[i].Private = stringToKey(v.Private, dh.SizeKey255)
+		vectors[i].Shared = stringToKey(v.Shared, dh.SizeKey255)
+	}
+	return vectors
+}
+
+// TestWycheproof verifies test vectors from Wycheproof v0.4.12
+func TestWycheproof(t *testing.T) {
+	vectors := readVectors(t)
+	for _, v := range vectors {
+		want := v.Shared
+		got := v.Private.Shared(v.Public)
+		if got != want {
+			t.Errorf("Failed id: %v\ngot: %v\nwant:%v\n", v.TcId, got, want)
+		}
+	}
+}
