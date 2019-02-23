392a97d14c50bd137e05da34365f6d9e790ae1e0
Max Semenik <maxsem.wiki@gmail.com>
2019-02-23 00:20:48 -0800

    ssh/terminal: fix GetSize on Windows
    Return window size instead of buffer size.
    
    Fixes golang/go#27743
    
    Change-Id: Ib1cd249f5680d86d505032e51d9102c2718ddf6f

--- ssh/terminal/util_windows.go
+++ ssh/terminal/util_windows.go
@@ -64,13 +64,15 @@ func Restore(fd int, state *State) error {
 	return windows.SetConsoleMode(windows.Handle(fd), state.mode)
 }
 
-// GetSize returns the dimensions of the given terminal.
+// GetSize returns the visible dimensions of the given terminal.
+//
+// These dimensions don't include screen buffer which is the overall scrollable size.
 func GetSize(fd int) (width, height int, err error) {
 	var info windows.ConsoleScreenBufferInfo
 	if err := windows.GetConsoleScreenBufferInfo(windows.Handle(fd), &info); err != nil {
 		return 0, 0, err
 	}
-	return int(info.Size.X), int(info.Size.Y), nil
+	return int(info.Window.Right - info.Window.Left), int(info.Window.Bottom - info.Window.Top), nil
 }
 
 // ReadPassword reads a line of input from a terminal without local echo.  This
