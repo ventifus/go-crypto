379efb39f79a225b5aab43babbcbddcb524d0cf6
ibuler <ibuler@qq.com>
2019-05-09 06:13:04 +0000

    ssh: add ssh partial success for ssh server 2FA
    Adds ssh partial success error, and next auth methods call back, When auth methods call back return ErrPartialSuccess, then let client continue use nextAuthMethodsCallback return methods auth farther
    
    I don't know if match RFC or not, Please check
    
    #https://github.com/golang/go/issues/17889
    #https://github.com/golang/go/issues/17934
    
    Change-Id: I5d9eaaa9e21f3a2b9ebbcdd4a5779502096512eb
    GitHub-Last-Rev: ef0d1a6f5b87067803518089d4cbc349777a56bd
    GitHub-Pull-Request: golang/crypto#89

--- ssh/server.go
+++ ssh/server.go
@@ -85,6 +85,11 @@ type ServerConfig struct {
 	// unknown.
 	KeyboardInteractiveCallback func(conn ConnMetadata, client KeyboardInteractiveChallenge) (*Permissions, error)
 
+	// NextAuthMethodsCallback if non-nil, is called when auth methods
+	// return PartialSuccess error, then these methods will be
+	// methods auth can continue
+	NextAuthMethodsCallback func(conn ConnMetadata) []string
+
 	// AuthLogCallback, if non-nil, is called to log all authentication
 	// attempts.
 	AuthLogCallback func(conn ConnMetadata, method string, err error)
@@ -320,6 +325,11 @@ func (l ServerAuthError) Error() string {
 // It is returned in ServerAuthError.Errors from NewServerConn.
 var ErrNoAuth = errors.New("ssh: no auth passed yet")
 
+// ErrPartialSuccess is the error some auth method partially successful
+// but need more auth method confirm, If return, then config.NextAuthMethodsCallback
+// will be call, and check it
+var ErrPartialSuccess = errors.New("authenticated with partial success")
+
 func (s *connection) serverAuthenticate(config *ServerConfig) (*Permissions, error) {
 	sessionID := s.transport.getSessionID()
 	var cache pubKeyCache
@@ -328,6 +338,9 @@ func (s *connection) serverAuthenticate(config *ServerConfig) (*Permissions, err
 	authFailures := 0
 	var authErrs []error
 	var displayedBanner bool
+	var firstAuthOk bool
+	var nextAuthMethods []string
+	var nextAuthMethodsPlain string
 
 userAuthLoop:
 	for {
@@ -375,6 +388,16 @@ userAuthLoop:
 
 		perms = nil
 		authErr := ErrNoAuth
+		// get next auth methods
+		if len(nextAuthMethods) == 0 && config.NextAuthMethodsCallback != nil {
+			nextAuthMethods = config.NextAuthMethodsCallback(s)
+			nextAuthMethodsPlain = strings.Join(nextAuthMethods, ",")
+		}
+		// If user request auth method in next auth method, should be deny
+		if strings.Contains(nextAuthMethodsPlain, userAuthReq.Method) && !firstAuthOk {
+			authErr = errors.New(fmt.Sprintf("ssh: %v auth should be first auth success", nextAuthMethods))
+			break
+		}
 
 		switch userAuthReq.Method {
 		case "none":
@@ -513,18 +536,29 @@ userAuthLoop:
 		authFailures++
 
 		var failureMsg userAuthFailureMsg
-		if config.PasswordCallback != nil {
+
+		// if password in next auth methods, should not be tell client
+		if config.PasswordCallback != nil && !strings.Contains(nextAuthMethodsPlain, "password") {
 			failureMsg.Methods = append(failureMsg.Methods, "password")
 		}
-		if config.PublicKeyCallback != nil {
+		// if publickey in next auth methods, should not be tell client
+		if config.PublicKeyCallback != nil && !strings.Contains(nextAuthMethodsPlain, "publickey") {
 			failureMsg.Methods = append(failureMsg.Methods, "publickey")
 		}
-		if config.KeyboardInteractiveCallback != nil {
+		// if keyboard-interactive in next auth methods, should not be tell client
+		if config.KeyboardInteractiveCallback != nil && !strings.Contains(nextAuthMethodsPlain, "keyboard-interactive") {
 			failureMsg.Methods = append(failureMsg.Methods, "keyboard-interactive")
 		}
 
 		if len(failureMsg.Methods) == 0 {
 			return nil, errors.New("ssh: no authentication methods configured but NoClientAuth is also false")
+		}
+
+		// if auth error is partial success, so need next auth
+		if authErr == ErrPartialSuccess && len(nextAuthMethods) > 0 {
+			firstAuthOk = true
+			failureMsg.PartialSuccess = true
+			failureMsg.Methods = nextAuthMethods
 		}
 
 		if err := s.transport.writePacket(Marshal(&failureMsg)); err != nil {
