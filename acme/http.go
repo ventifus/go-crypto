b1028f15f288afa78427745a475cca94eba201d8
Alex Vaghin <alex@cloudware.io>
2019-10-03 07:49:50 +0200

    acme: keep custom User-Agent as is
    A golang.org/cl/183267 added User-Agent header to help CAs identify
    their users but unfortunately it broke some users of the Client which rely on
    a custom User-Agent being sent as is, without modifications.
    
    Updates golang/go#24496
    
    Change-Id: If829176ec34dddaa2f0d199934c3b11ba9a43929

--- acme/http.go
+++ acme/http.go
@@ -270,17 +270,17 @@ func (c *Client) httpClient() *http.Client {
 // sending as part of the User-Agent header. It's set in version_go112.go.
 var packageVersion string
 
-// userAgent returns the User-Agent header value. It includes the package name,
-// the module version (if available), and the c.UserAgent value (if set).
+// userAgent returns the User-Agent header value.
+// It returns c.UserAgent as is if non-empty.
+// Otherwise, it includes the package name and the module version (if available).
 func (c *Client) userAgent() string {
-	ua := "golang.org/x/crypto/acme"
-	if packageVersion != "" {
-		ua += "@" + packageVersion
-	}
 	if c.UserAgent != "" {
-		ua = c.UserAgent + " " + ua
+		return c.UserAgent
 	}
-	return ua
+	if packageVersion == "" {
+		return "golang.org/x/crypto/acme"
+	}
+	return "golang.org/x/crypto/acme@" + packageVersion
 }
 
 // isBadNonce reports whether err is an ACME "badnonce" error.
