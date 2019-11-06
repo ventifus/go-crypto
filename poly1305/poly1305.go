7347fd8dd1b83f1535fe7c2e22250e6ba14b8541
Filippo Valsorda <filippo@golang.org>
2019-11-06 18:01:18 -0500

    poly1305: add (*MAC).Verify API and use it in chacha20poly1305
    DO NOT REVIEW: this is broken! Also, it will have to wait until the next
    cycle, as it came after the freeze.
    
    Change-Id: I2c30ddc960a889b49c8ee8ff8073ffc4e75f43af

--- poly1305/poly1305.go
+++ poly1305/poly1305.go
@@ -58,7 +58,7 @@ func New(key *[32]byte) *MAC {
 // MAC cannot be used like common hash.Hash implementations,
 // because using a poly1305 key twice breaks its security.
 // Therefore writing data to a running MAC after calling
-// Sum causes it to panic.
+// Sum or Verify causes it to panic.
 type MAC struct {
 	mac // platform-dependent implementation
 
@@ -71,10 +71,10 @@ func (h *MAC) Size() int { return TagSize }
 // Write adds more data to the running message authentication code.
 // It never returns an error.
 //
-// It must not be called after the first call of Sum.
+// It must not be called after the first call of Sum or Verify.
 func (h *MAC) Write(p []byte) (n int, err error) {
 	if h.finalized {
-		panic("poly1305: write to MAC after Sum")
+		panic("poly1305: write to MAC after Sum or Verify")
 	}
 	return h.mac.Write(p)
 }
@@ -86,4 +86,13 @@ func (h *MAC) Sum(b []byte) []byte {
 	h.mac.Sum(&mac)
 	h.finalized = true
 	return append(b, mac[:]...)
+}
+
+// Verify returns whether the authenticator of all data written to
+// the message authentication code matches the expected value.
+func (h *MAC) Verify(expected []byte) bool {
+	var mac [TagSize]byte
+	h.mac.Sum(&mac)
+	h.finalized = true
+	return subtle.ConstantTimeCompare(expected, mac[:]) == 1
 }
