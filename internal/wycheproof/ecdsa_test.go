a88748dbef6e31d38680b22996b0a23671e4f485
Katie Hockman <katie@golang.org>
2019-11-27 15:17:25 -0500

    internal/wycheproof: add Wycheproof tests for verifying signatures
    https://github.com/google/wycheproof provides test vectors exposing
    vulnerabilities in crypto packages. This change creates a new directory
    called internal/wycheproof that runs these Wycheproof tests against
    a number of crypto libraries.
    
    Directory structure:
     - interal/wycheproof/ecdsa: internal version of ecdsa package which
    includes a new function that verifies ASN encoded signatures directly
     - interal/wycheproof/dsa: internal version of dsa package which
    includes a new function that verifies ASN encoded signatures directly
     - internal/wycheproof/vendor: vendored version of files copied from
    github.com/google/wycheproof using the script in the README
     - internal/wycherpoof: all tests
    
    internal/wycheproof/wycheproof.go provides utility functions that are
    common to many tests in the package.
    
    This change includes tests for signature verification with dsa, ecdsa,
    eddsa, and rsa (both PKCS#1 v1.5 and PSS signatures).
    
    More tests will be added incrementally.
    
    Change-Id: I0378d4be24b5679fdc186e9fc94c1cc0068e81f7

---
+++ internal/wycheproof/ecdsa_test.go
@@ -0,0 +1,147 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wycheproof
+
+import (
+	"crypto/ecdsa"
+	"encoding/json"
+	"testing"
+
+	wecdsa "golang.org/x/crypto/internal/wycheproof/internal/ecdsa"
+)
+
+func TestEcdsa(t *testing.T) {
+	// flagsShouldPass is a map associated with whether or not
+	// a flag for an "acceptable" result should pass.
+	// Every possible flag value that's associated with an
+	// "acceptable" result should be explicitly specified,
+	// otherwise the test will panic.
+	flagsShouldPass := map[string]bool{
+		"EdgeCase":    true,
+		"MissingZero": false, // An encoded integer missing a leading zero can lead to an invalid signature.
+		"WeakHash":    true,  // A signature using a weaker hash than the EC params is not a security risk.
+	}
+
+	// supportedCurves is a map of all elliptic curves supported
+	// by crypto/elliptic, which can subsequently be parsed and tested.
+	supportedCurves := map[string]bool{
+		"secp224r1": true,
+		"secp256r1": true,
+		"secp384r1": true,
+		"secp521r1": true,
+	}
+
+	b := readTestVector("ecdsa_test.json")
+	var root EcdsaRoot
+	if err := json.Unmarshal(b, &root); err != nil {
+		t.Fatalf("failed to unmarshal json file: %v", err)
+	}
+	for _, tg := range root.TestGroups {
+		curve := tg.Key.Curve.(string)
+		if !supportedCurves[curve] {
+			continue
+		}
+		pub := decodeKey(tg.KeyDer).(*ecdsa.PublicKey)
+		h, _ := parseHash(tg.Sha)
+		for _, sig := range tg.Tests {
+			h.Reset()
+			h.Write(decodeHex(sig.Msg))
+			hashed := h.Sum(nil)
+			result := wecdsa.VerifyASN1(pub, hashed, decodeHex(sig.Sig))
+			if got, want := result, shouldPass(sig.Result, sig.Flags, flagsShouldPass); got != want {
+				t.Errorf("tcid=%d, type: %s, comment: %q, wanted success = %t", sig.TcId, sig.Result, sig.Comment, want)
+			}
+		}
+	}
+}
+
+// EcPublicKey
+type EcPublicKey struct {
+
+	// the EC group used by this public key
+	Curve interface{} `json:"curve,omitempty"`
+
+	// the key size in bits
+	KeySize int `json:"keySize,omitempty"`
+
+	// the key type
+	Type string `json:"type,omitempty"`
+
+	// encoded public key point
+	Uncompressed string `json:"uncompressed,omitempty"`
+
+	// the x-coordinate of the public key point
+	Wx string `json:"wx,omitempty"`
+
+	// the y-coordinate of the public key point
+	Wy string `json:"wy,omitempty"`
+}
+
+// EcUnnamedGroup
+type EcUnnamedGroup struct {
+
+	// coefficient a of the elliptic curve equation
+	A string `json:"a,omitempty"`
+
+	// coefficient b of the elliptic curve equation
+	B string `json:"b,omitempty"`
+
+	// the x-coordinate of the generator
+	Gx string `json:"gx,omitempty"`
+
+	// the y-coordinate of the generator
+	Gy string `json:"gy,omitempty"`
+
+	// the cofactor
+	H int `json:"h,omitempty"`
+
+	// the order of the generator
+	N string `json:"n,omitempty"`
+
+	// the order of the underlying field
+	P string `json:"p,omitempty"`
+
+	// an unnamed EC group over a prime field in Weierstrass form
+	Type string `json:"type,omitempty"`
+}
+
+// EcdsaTestGroup
+type EcdsaTestGroup struct {
+
+	// unenocded EC public key
+	Key *EcPublicKey `json:"key,omitempty"`
+
+	// DER encoded public key
+	KeyDer string `json:"keyDer,omitempty"`
+
+	// Pem encoded public key
+	KeyPem string `json:"keyPem,omitempty"`
+
+	// the hash function used for ECDSA
+	Sha   string                 `json:"sha,omitempty"`
+	Tests []*SignatureTestVector `json:"tests,omitempty"`
+	Type  interface{}            `json:"type,omitempty"`
+}
+
+// EcdsaRoot
+type EcdsaRoot struct {
+
+	// the primitive tested in the test file
+	Algorithm string `json:"algorithm,omitempty"`
+
+	// the version of the test vectors.
+	GeneratorVersion string `json:"generatorVersion,omitempty"`
+
+	// additional documentation
+	Header []string `json:"header,omitempty"`
+
+	// a description of the labels used in the test vectors
+	Notes *Notes `json:"notes,omitempty"`
+
+	// the number of test vectors in this test
+	NumberOfTests int               `json:"numberOfTests,omitempty"`
+	Schema        interface{}       `json:"schema,omitempty"`
+	TestGroups    []*EcdsaTestGroup `json:"testGroups,omitempty"`
+}
