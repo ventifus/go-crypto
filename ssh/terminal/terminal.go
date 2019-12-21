a327e44657204db56ecbe9f7eebdbc15b8eda249
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-22 03:38:13 +0900

    ssh/terminal: stop using ENABLE_LINE_INPUT
    ReadConsole does not read more than 254 bytes when ENABLE_LINE_INPUT is
    enabled.
    
    Fixes golang/go#36071
    
    Change-Id: If5c160404b855387a80f1d57638aac3f2db1a097

--- ssh/terminal/terminal.go
+++ ssh/terminal/terminal.go
@@ -947,10 +947,15 @@ func readPasswordLine(reader io.Reader) ([]byte, error) {
 		n, err := reader.Read(buf[:])
 		if n > 0 {
 			switch buf[0] {
+			case '\b':
+				if len(ret) > 0 {
+					ret = ret[:len(ret)-1]
+				}
 			case '\n':
 				return ret, nil
 			case '\r':
-				// remove \r from passwords on Windows
+				// Windows sends \r at the end of input when ENABLE_LINE_INPUT
+				// is disabled. But reader may have next \n.
 			default:
 				ret = append(ret, buf[0])
 			}
