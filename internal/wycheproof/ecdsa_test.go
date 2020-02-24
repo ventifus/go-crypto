bb98293d6a4bfec54b4c5f045f87918ccf4e2cab
Katie Hockman <katie@golang.org>
2020-02-24 16:46:38 -0500

    internal/wycheproof: update TestEcdsa to use ecdsa.VerifyASN1
    Change-Id: Ibd6ce156550615cb85c06e734641c34fca0cfcd0

--- internal/wycheproof/ecdsa_test.go
+++ internal/wycheproof/ecdsa_test.go
@@ -2,13 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !go1.15
+
+// ecdsa.VerifyASN1 was added in Go 1.15.
+
 package wycheproof
 
 import (
 	"crypto/ecdsa"
 	"testing"
-
-	wecdsa "golang.org/x/crypto/internal/wycheproof/internal/ecdsa"
 )
 
 func TestEcdsa(t *testing.T) {
@@ -157,7 +159,7 @@ func TestEcdsa(t *testing.T) {
 			h.Reset()
 			h.Write(decodeHex(sig.Msg))
 			hashed := h.Sum(nil)
-			got := wecdsa.VerifyASN1(pub, hashed, decodeHex(sig.Sig))
+			got := verifyASN1(pub, hashed, decodeHex(sig.Sig))
 			if want := shouldPass(sig.Result, sig.Flags, flagsShouldPass); got != want {
 				t.Errorf("tcid: %d, type: %s, comment: %q, wanted success: %t", sig.TcId, sig.Result, sig.Comment, want)
 			}
