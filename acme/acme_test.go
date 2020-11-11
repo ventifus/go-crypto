d705dbd61455d7245c8b174268f8b947578086eb
James Kasten <jdkasten@google.com>
2020-11-11 15:47:14 -0800

    acme: add external account binding support
    Implements https://tools.ietf.org/html/rfc8555#section-7.3.4
    
    Fixes golang/go#41430
    
    Co-authored-by: James Munnelly <james@munnelly.eu>
    Change-Id: Icd0337fddbff49e7e79fb9105c2679609f990285

--- acme/acme_test.go
+++ acme/acme_test.go
@@ -188,6 +188,17 @@ func TestRegister(t *testing.T) {
 	}
 }
 
+func TestRegisterError(t *testing.T) {
+	c := &Client{}
+	wantErr := "acme: client.Key must be set to Register"
+	_, err := c.Register(context.Background(), &Account{}, AcceptTOS)
+	if err == nil {
+		t.Errorf("c.Register() with empty client succeeded, wanted error")
+	} else if err.Error() != wantErr {
+		t.Errorf("c.Register() with empty client = %v, want error %q", err, wantErr)
+	}
+}
+
 func TestUpdateReg(t *testing.T) {
 	const terms = "https://ca.tld/acme/terms"
 	contacts := []string{"mailto:admin@example.com"}
