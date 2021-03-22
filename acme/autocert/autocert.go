1aed1ec4db8e69c4eaec61cb6d29e9ea4f4898c1
Trevor Dixon <trevordixon@gmail.com>
2021-03-22 12:53:14 +0000

    acme/autocert: include the domain in the SAN of the CSR
    More compliant with the spec and allows autocert to work with Pebble (see https://github.com/letsencrypt/pebble/issues/304). Fixes https://github.com/golang/go/issues/39746.
    
    Change-Id: I0f41d5b41800d57eb53055cad248e50573c6070f
    GitHub-Last-Rev: 9860d605a5042280652d0aeeddc1f24782619181
    GitHub-Pull-Request: golang/crypto#143

--- acme/autocert/autocert.go
+++ acme/autocert/autocert.go
@@ -645,7 +645,7 @@ func (m *Manager) certState(ck certKey) (*certState, error) {
 // authorizedCert starts the domain ownership verification process and requests a new cert upon success.
 // The key argument is the certificate private key.
 func (m *Manager) authorizedCert(ctx context.Context, key crypto.Signer, ck certKey) (der [][]byte, leaf *x509.Certificate, err error) {
-	csr, err := certRequest(key, ck.domain, m.ExtraExtensions)
+	csr, err := certRequest(key, ck.domain, m.ExtraExtensions, ck.domain)
 	if err != nil {
 		return nil, nil, err
 	}
