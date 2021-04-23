706e1bcc49768a94307233ce7a456ba18c61153f
Sean Liao <seankhliao@gmail.com>
2021-04-23 21:28:42 +0200

    sha3: fix state.Write doc
    Use the ShakeHash Write doc for state.Write
    
    Fixes golang/go#45683
    
    Change-Id: I5e7cadca964078f7f0a4500f90680267e71a0acb

--- sha3/sha3.go
+++ sha3/sha3.go
@@ -121,8 +121,8 @@ func (d *state) padAndPermute(dsbyte byte) {
 	copyOut(d, d.buf)
 }
 
-// Write absorbs more data into the hash's state. It produces an error
-// if more data is written to the ShakeHash after writing
+// Write absorbs more data into the hash's state. It panics if input is
+// written to it after output has been read from it.
 func (d *state) Write(p []byte) (written int, err error) {
 	if d.state != spongeAbsorbing {
 		panic("sha3: write to sponge after read")
