37b22b1e3f055c08884976d4b21072a14bd767c8
Matthew Dempsky <mdempsky@google.com>
2021-04-28 14:28:45 -0700

    curve25519: use fiat-go
    DO NOT SUBMIT -- not working yet
    
    Change-Id: Iaa263d6ffabf324ec32a08f03de6d23a0d3a74d6

---
+++ curve25519/curve25519_fiat64.go
@@ -0,0 +1,36 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package curve25519
+
+import (
+	"github.com/mit-plv/fiat-crypto/fiat-go/64/curve25519"
+)
+
+type fieldElement = [5]uint64
+
+func feFromBytes(dst *fieldElement, src *[32]byte) { curve25519.FromBytes(dst, src) }
+func feToBytes(dst *[32]byte, src *fieldElement)   { curve25519.ToBytes(dst, src) }
+
+func feZero(fe *fieldElement)       { *fe = fieldElement{} }
+func feOne(fe *fieldElement)        { *fe = fieldElement{1} }
+func feCopy(dst, src *fieldElement) { *dst = *src }
+
+func feAdd(dst, a, b *fieldElement)  { curve25519.Add(dst, a, b) }
+func feSub(dst, a, b *fieldElement)  { curve25519.Sub(dst, a, b) }
+func feMul(h, f, g *fieldElement)    { curve25519.CarryMul(h, f, g) }
+func feSquare(h, f *fieldElement)    { curve25519.CarrySquare(h, f) }
+func feMul121666(h, f *fieldElement) { curve25519.CarryScmul121666(h, f) }
+
+// feCSwap replaces (f,g) with (g,f) if b == 1; replaces (f,g) with (f,g) if b == 0.
+//
+// Preconditions: b in {0,1}.
+func feCSwap(f, g *fieldElement, swap byte) {
+	b := -uint64(swap)
+	for i := range f {
+		t := b & (f[i] ^ g[i])
+		f[i] ^= t
+		g[i] ^= t
+	}
+}
