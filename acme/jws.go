9dce4a5de2012bc9bbbbfd409e86764993b50691
Brad Fitzpatrick <bradfitz@golang.org>
2021-08-15 22:16:55 -0700

    acme: remove support for pre-RFC 8555 ACME spec
    LetsEncrypt removed it anyway.
    
    No API changes. Just a lot of deleted code.
    
    Fixes golang/go#46654
    
    Change-Id: I6e22f95bbc771abde8a445d7a7cc09d11f5eff93

--- acme/jws.go
+++ acme/jws.go
@@ -51,6 +51,9 @@ type jsonWebSignature struct {
 //
 // See https://tools.ietf.org/html/rfc7515#section-7.
 func jwsEncodeJSON(claimset interface{}, key crypto.Signer, kid keyID, nonce, url string) ([]byte, error) {
+	if key == nil {
+		panic("nil key")
+	}
 	alg, sha := jwsHasher(key.Public())
 	if alg == "" || !sha.Available() {
 		return nil, ErrUnsupportedKey
