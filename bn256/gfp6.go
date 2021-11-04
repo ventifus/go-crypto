408468f81772d9905ebf89f522e559062aa3ad3a
Koji Aomatsu <penbook.koji@gmail.com>
2021-11-05 01:28:11 +0900

    crypto: fix wrong formulas in comments
    Fixes golang/go#48143
    
    Change-Id: I282c69b8d9f13c4ec943ae2df1f60eb74409cd9e

--- bn256/gfp6.go
+++ bn256/gfp6.go
@@ -249,9 +249,9 @@ func (e *gfP6) Invert(a *gfP6, pool *bnPool) *gfP6 {
 	// = (x³ξ²+y³ξ+z³-3ξxyz) = F is an element of the base field (the norm).
 	//
 	// On the other hand (xj²τ² + yjτ + z)(xjτ² + yj²τ + z)
-	// = τ²(y²-ξxz) + τ(ξx²-yz) + (z²-ξxy)
+	// = τ²(y²-xz) + τ(ξx²-yz) + (z²-ξxy)
 	//
-	// So that's why A = (z²-ξxy), B = (ξx²-yz), C = (y²-ξxz)
+	// So that's why A = (z²-ξxy), B = (ξx²-yz), C = (y²-xz)
 	t1 := newGFp2(pool)
 
 	A := newGFp2(pool)
