b4b29a8b8707a77481a27805f6af1291787ca0cf
umlublin <pilne1407@gmail.com>
2021-11-19 17:10:13 +0100

    Fixes golang/go#49678 x/crypto/cryptobyte/asn1: Error parsing ASN1 identifiers
    Change-Id: If8a40e25edd810a66165ab78dd68d9b7fc2699f8

--- cryptobyte/asn1.go
+++ cryptobyte/asn1.go
@@ -407,7 +407,11 @@ func (s *String) ReadASN1Enum(out *int) bool {
 func (s *String) readBase128Int(out *int) bool {
 	ret := 0
 	for i := 0; len(*s) > 0; i++ {
-		if i == 4 {
+		if i == 5 {
+			return false
+		}
+		// OID arc size should be lower than 2**31-1
+		if ret > (1<<(31-7) - 1) {
 			return false
 		}
 		ret <<= 7
