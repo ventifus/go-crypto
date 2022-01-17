354786d04dabdaccbbccab4a0d82f8c7b8aae5a1
Eric Lagergren <eric@ericlagergren.com>
2022-01-16 20:18:34 -0800

    blake2b: implement ARMv8 assembly core
    ```
    name        old time/op   new time/op   delta
    Write128-8    173ns ± 0%    145ns ± 0%  -16.40%  (p=0.000 n=10+9)
    Write1K-8    1.34µs ± 0%   1.14µs ± 0%  -15.02%  (p=0.000 n=6+9)
    Sum128-8      191ns ± 0%    149ns ± 0%  -21.75%  (p=0.000 n=9+10)
    Sum1K-8      1.36µs ± 0%   1.14µs ± 0%  -16.11%  (p=0.000 n=10+10)
    
    name        old speed     new speed     delta
    Write128-8  740MB/s ± 0%  886MB/s ± 0%  +19.60%  (p=0.000 n=10+9)
    Write1K-8   762MB/s ± 0%  896MB/s ± 0%  +17.67%  (p=0.000 n=9+10)
    Sum128-8    671MB/s ± 0%  857MB/s ± 0%  +27.79%  (p=0.000 n=9+10)
    Sum1K-8     752MB/s ± 0%  896MB/s ± 0%  +19.21%  (p=0.000 n=10+10)
    ```
    
    Change-Id: I571f6dd9b08743555dcbd50cd2387fd8b77e39ae

--- blake2b/blake2b_ref.go
+++ blake2b/blake2b_ref.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !amd64 || purego || !gc
-// +build !amd64 purego !gc
+//go:build !(arm64 || amd64) || purego || !gc
+// +build !arm64,!amd64 purego !gc
 
 package blake2b
 
