6e2a4bc9f1014ef356d7e4777306b245c21d6b2f
Eric Lagergren <eric@ericlagergren.com>
2022-05-05 21:09:53 -0700

    x/crypto: add AES-GCM-SIV
    Fixes golang/go#54364
    
    Change-Id: I9d5efa7265c1c52fe750f1decadecfe319e1af7b

---
+++ aesgcmsiv/aesgcmsiv_noasm.go
@@ -0,0 +1,19 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !(amd64 || arm64) || !gc || purego
+
+package aesgcmsiv
+
+func (a *aead) seal(out, nonce, plaintext, additionalData []byte) {
+	a.sealGeneric(out, nonce, plaintext, additionalData)
+}
+
+func (a *aead) open(out, nonce, ciphertext, tag, additionalData []byte) bool {
+	return a.openGeneric(out, nonce, ciphertext, tag, additionalData)
+}
+
+func deriveKeys(authKey *[24]byte, encKey *[40]byte, keyGenKey, nonce []byte) {
+	deriveKeysGeneric(authKey, encKey, keyGenKey, nonce)
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.17
 require (
 	golang.org/x/exp v0.0.0-20220428152302-39d4317da171
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
-	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 )
 
