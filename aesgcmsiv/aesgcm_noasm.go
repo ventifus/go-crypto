13936dbbc744ccf679e340187c37e89eb4574c2b
Eric Lagergren <eric@ericlagergren.com>
2022-05-05 21:09:53 -0700

    crypto: add AES-GCM-SIV
    TODO:
    
    Fixes XXX
    
    Change-Id: I9d5efa7265c1c52fe750f1decadecfe319e1af7b

---
+++ aesgcmsiv/aesgcm_noasm.go
@@ -0,0 +1,13 @@
+package aesgcmsiv
+
+func (a *aead) seal(out, nonce, plaintext, additionalData []byte) {
+	a.sealGeneric(out, nonce, plaintext, additionalData)
+}
+
+func (a *aead) open(out, nonce, ciphertext, tag, additionalData []byte) bool {
+	return a.openGeneric(out, nonce, ciphertext, tag, additionalData)
+}
+
+func deriveKeys(authKey *[24]byte, encKey *[40]byte, keyGenKey, nonce []byte) {
+	deriveKeysGeneric(authKey, encKey, keyGenKey, nonce)
+}
