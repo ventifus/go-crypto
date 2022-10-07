3d183850d25e8b9f8870e939d219722b090f582b
Alberto Bertogli <albertito@blitiri.com.ar>
2022-10-07 13:39:19 +0100

    acme/autocert: use standard functions to pick the cache directory
    acme/autocert currently has ad-hoc logic to find a reasonable default
    for a cache directory.
    
    Since that logic was written (in 2017), new functions were added to the
    os package to provide that functionality (in Go 1.13, 2019-09):
    `os.UserCacheDir` and `os.UserHomeDir`.
    
    This patch replaces the ad-hoc logic with calls to `os.UserCacheDir` and
    `os.UserHomeDir`.
    
    The fallback to `/` is kept, since it may be relied upon in some
    environments.
    
    Change-Id: I3bf692ca670b87bf3d329e5d3684eee15ed374aa

--- acme/autocert/listener.go
+++ acme/autocert/listener.go
@@ -10,7 +10,6 @@ import (
 	"net"
 	"os"
 	"path/filepath"
-	"runtime"
 	"time"
 )
 
@@ -124,32 +123,17 @@ func (ln *listener) Close() error {
 	return ln.tcpListener.Close()
 }
 
-func homeDir() string {
-	if runtime.GOOS == "windows" {
-		return os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-	}
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return "/"
-}
-
 func cacheDir() string {
 	const base = "golang-autocert"
-	switch runtime.GOOS {
-	case "darwin":
-		return filepath.Join(homeDir(), "Library", "Caches", base)
-	case "windows":
-		for _, ev := range []string{"APPDATA", "CSIDL_APPDATA", "TEMP", "TMP"} {
-			if v := os.Getenv(ev); v != "" {
-				return filepath.Join(v, base)
-			}
-		}
-		// Worst case:
-		return filepath.Join(homeDir(), base)
+	if cd, _ := os.UserCacheDir(); cd != "" {
+		return filepath.Join(cd, base)
 	}
-	if xdg := os.Getenv("XDG_CACHE_HOME"); xdg != "" {
-		return filepath.Join(xdg, base)
+
+	// Fall back to the home directory.
+	if hd, _ := os.UserHomeDir(); hd != "" {
+		return filepath.Join(hd, ".cache", base)
 	}
-	return filepath.Join(homeDir(), ".cache", base)
+
+	// Fall back to the root directory.
+	return filepath.Join("/.cache", base)
 }
