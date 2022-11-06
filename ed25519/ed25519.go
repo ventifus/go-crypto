a2d4f8e00dde6c4d760728aa0574548ed518e16d
Sebastiaan van Stijn <github@gone.nl>
2022-11-06 15:32:03 +0000

    ed25519: make consts an alias for crypto/ed25519
    This is a follow-up to Change-Id: I860ed963a0660753d01c89014c21360b239a38ac (commit 20e1d8d225ab8b19a0e7dc00fa5095216b8d6553), which dropped compatibility with go1.12, and made this package an alias / wrapper for crypto/ed25519 in stdlib.
    
    While the above change aliased types and created wrappers for functions, it left out the consts. I looked at the code review and commit message on that change, but did not find a specific reason to leave these out. This patch updates the consts to be an alias as well, which should make it even more transparent that this package is an alias.
    
    Change-Id: I2e2a75dcb8a507451844d25b7afa6a935f5de18b
    GitHub-Last-Rev: 070f3a658bdd92974ba315321c18a4198ba9593d
    GitHub-Pull-Request: golang/crypto#237

--- ed25519/ed25519.go
+++ ed25519/ed25519.go
@@ -23,13 +23,21 @@ import (
 
 const (
 	// PublicKeySize is the size, in bytes, of public keys as used in this package.
-	PublicKeySize = 32
+	//
+	// This const is an alias for [crypto/ed25519.PublicKeySize].
+	PublicKeySize = ed25519.PublicKeySize
 	// PrivateKeySize is the size, in bytes, of private keys as used in this package.
-	PrivateKeySize = 64
+	//
+	// This const is an alias for [crypto/ed25519.PrivateKeySize].
+	PrivateKeySize = ed25519.PrivateKeySize
 	// SignatureSize is the size, in bytes, of signatures generated and verified by this package.
-	SignatureSize = 64
+	//
+	// This const is an alias for [crypto/ed25519.SignatureSize].
+	SignatureSize = ed25519.SignatureSize
 	// SeedSize is the size, in bytes, of private key seeds. These are the private key representations used by RFC 8032.
-	SeedSize = 32
+	//
+	// This const is an alias for [crypto/ed25519.SeedSize].
+	SeedSize = ed25519.SeedSize
 )
 
 // PublicKey is the type of Ed25519 public keys.
