e5bb276f18a92a151dd16ae1ea3589473ebf4828
Roland Shoemaker <roland@golang.org>
2023-01-13 14:52:50 -0800

    x509roots: add new module
    Adds the nss parser, under x509roots/nss, and the fallback
    module/package, with the initial generated bundle.
    
    Fixes golang/go#57792
    
    Change-Id: Iebb1052e49126fa5baba1236f4ebc8dd8a823179

---
+++ x509roots/gen.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build go1.20
+// +build go1.20
+
+//go:generate go run gen_fallback_bundle.go
+
+package x509roots
