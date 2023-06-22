5407a491ed180145579d2400b99db474e34d7e9f
Heschi Kreinick <heschi@google.com>
2023-06-22 17:03:34 -0400

    ssh/test: increase sshd verbosity
    Change-Id: I536b8a0567e786961e585084b7807ae6972949d8

--- ssh/agent/client_test.go
+++ ssh/agent/client_test.go
@@ -35,6 +35,12 @@ func startOpenSSHAgent(t *testing.T) (client ExtendedAgent, socket string, clean
 		t.Skip("could not find ssh-agent")
 	}
 
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/C", bin, "-d", "-s")
+		out, err := cmd.CombinedOutput()
+		t.Logf("%v -d -s: %v, %v", bin, string(out), err)
+	}
+
 	cmd := exec.Command(bin, "-s")
 	cmd.Env = []string{} // Do not let the user's environment influence ssh-agent behavior.
 	cmd.Stderr = new(bytes.Buffer)
