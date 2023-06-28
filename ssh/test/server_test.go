29046f41856f23ea4f3eb69f998af760f59753b4
Nicola Murino <nicola.murino@gmail.com>
2023-06-28 18:30:13 +0200

    ssh: add test case against ssh CLI
    These tests try to ensure better compatibility of our server implementation
    with the ssh CLI.
    
    With these tests in place:
    
    1) before merging CL 447757 we would have noticed that our server
       implementation was broken with OpenSSH 8.8+
    2) after merging CL 447757 we would have noticed that our server
       implementation was broken with OpenSSH 7.2-7.7
    
    The ssh CLI from $PATH is used by default, but can be overridden using
    the SSH_CLI_PATH environment variable.
    
    Change-Id: I93d64be41c7613132b0364afac8397f57c2dcbca

---
+++ ssh/test/server_test.go
@@ -0,0 +1,108 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"net"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type exitStatusMsg struct {
+	Status uint32
+}
+
+// goServer is a test Go SSH server that accepts public key and certificate
+// authentication and replies with a 0 exit status to any exec request without
+// running any commands, e.g.
+//
+// ssh -i <pub key path> -p <server port> username@127.0.0.1 /bin/true
+//
+// If the client presents a certificate signed by testPublicKeys["ca"], it's
+// accepted for any user. If a public key is presented, it's accepted only if
+// it's testPublicKeys["rsa"] and the username is testpubkey. The server will
+// also store the number of successful public key and certificate
+// authentications.
+type goTestServer struct {
+	listener net.Listener
+	config   *ssh.ServerConfig
+	done     chan struct{}
+}
+
+func newTestServer(config *ssh.ServerConfig) (*goTestServer, string, error) {
+	server := &goTestServer{
+		config: config,
+	}
+	port, err := server.Start()
+	return server, port, err
+}
+
+func (s *goTestServer) Start() (string, error) {
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		return "", err
+	}
+	s.listener = listener
+	_, port, err := net.SplitHostPort(listener.Addr().String())
+	if err != nil {
+		return "", err
+	}
+	s.done = make(chan struct{}, 1)
+	go s.acceptConnections()
+	return port, nil
+}
+
+func (s *goTestServer) acceptConnections() {
+	defer close(s.done)
+
+	for {
+		c, err := s.listener.Accept()
+		if err != nil {
+			return
+		}
+		_, chans, reqs, err := ssh.NewServerConn(c, s.config)
+		if err != nil {
+			return
+		}
+		go ssh.DiscardRequests(reqs)
+		defer c.Close()
+
+		for newChannel := range chans {
+			if newChannel.ChannelType() != "session" {
+				newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+				continue
+			}
+
+			channel, requests, err := newChannel.Accept()
+			if err != nil {
+				continue
+			}
+
+			go func(in <-chan *ssh.Request) {
+				for req := range in {
+					ok := false
+					switch req.Type {
+					case "exec":
+						ok = true
+						go func() {
+							channel.SendRequest("exit-status", false, ssh.Marshal(&exitStatusMsg{Status: 0}))
+							channel.Close()
+						}()
+					}
+					if req.WantReply {
+						req.Reply(ok, nil)
+					}
+				}
+			}(requests)
+		}
+	}
+}
+
+func (s *goTestServer) Close() error {
+	err := s.listener.Close()
+	// wait for the accept loop to exit
+	<-s.done
+	return err
+}
