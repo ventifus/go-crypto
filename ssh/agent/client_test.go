303e03929710f2897e4c42af5cf19bad7c8775d7
Nicola Murino <nicola.murino@gmail.com>
2023-07-12 13:21:21 +0200

    ssh: disable agent tests on Windows
    ssh-agent is implemented as a Windows service and exposed on
    a named pipe. We don't currently support it.
    
    See golang/go#60981
    
    Change-Id: Iebdc42db30b37a87ac0766231b16aff3f17b3f56

--- ssh/agent/client_test.go
+++ ssh/agent/client_test.go
@@ -29,6 +29,9 @@ func startOpenSSHAgent(t *testing.T) (client ExtendedAgent, socket string, clean
 		// types supported vary by platform.
 		t.Skip("skipping test due to -short")
 	}
+	if runtime.GOOS == "windows" {
+		t.Skip("skipping on windows")
+	}
 
 	bin, err := exec.LookPath("ssh-agent")
 	if err != nil {
