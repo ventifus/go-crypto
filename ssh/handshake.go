4c3640ad598696b987f7dc29d6b32e00cf3b61e5
Nicola Murino <nicola.murino@gmail.com>
2023-08-31 14:41:07 +0200

    ssh: add server side support for ping@openssh.com protocol extension
    Fixes golang/go#62390
    
    Change-Id: Ie4dc577fb55b45a0c26a9e2dc5903af2bd382e00

--- ssh/handshake.go
+++ ssh/handshake.go
@@ -645,13 +645,17 @@ func (t *handshakeTransport) enterKeyExchange(otherInitPacket []byte) error {
 	// RFC 8308, Sections 2.4 and 3.1.
 	if !isClient && firstKeyExchange && contains(clientInit.KexAlgos, "ext-info-c") {
 		extInfo := &extInfoMsg{
-			NumExtensions: 1,
-			Payload:       make([]byte, 0, 4+15+4+len(supportedPubKeyAuthAlgosList)),
+			NumExtensions: 2,
+			Payload:       make([]byte, 0, 4+15+4+len(supportedPubKeyAuthAlgosList)+4+16+4+1),
 		}
 		extInfo.Payload = appendInt(extInfo.Payload, len("server-sig-algs"))
 		extInfo.Payload = append(extInfo.Payload, "server-sig-algs"...)
 		extInfo.Payload = appendInt(extInfo.Payload, len(supportedPubKeyAuthAlgosList))
 		extInfo.Payload = append(extInfo.Payload, supportedPubKeyAuthAlgosList...)
+		extInfo.Payload = appendInt(extInfo.Payload, len("ping@openssh.com"))
+		extInfo.Payload = append(extInfo.Payload, "ping@openssh.com"...)
+		extInfo.Payload = appendInt(extInfo.Payload, 1)
+		extInfo.Payload = append(extInfo.Payload, "0"...)
 		if err := t.conn.writePacket(Marshal(extInfo)); err != nil {
 			return err
 		}
