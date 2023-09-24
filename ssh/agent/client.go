87577974dabb9443aa308fec83cdad65d28e8065
Nicola Murino <nicola.murino@gmail.com>
2023-09-24 12:41:23 +0200

    ssh: implement CryptoPublicKey on agent keys
    Implement the crypto.PublicKey interface for the default Agent
    implementation. CryptoPublicKey may return nil if the underlying
    ssh public key is not valid or does not implement the
    crypto.PublicKey interface
    
    Change-Id: I21123d5117313cfac43d7e3a779199e7a7d51fef

--- ssh/agent/client.go
+++ ssh/agent/client.go
@@ -14,6 +14,7 @@ package agent // import "golang.org/x/crypto/ssh/agent"
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/dsa"
 	"crypto/ecdsa"
 	"crypto/ed25519"
@@ -234,9 +235,10 @@ type extensionAgentMsg struct {
 // Key represents a protocol 2 public key as defined in
 // [PROTOCOL.agent], section 2.5.2.
 type Key struct {
-	Format  string
-	Blob    []byte
-	Comment string
+	Format    string
+	Blob      []byte
+	Comment   string
+	publicKey crypto.PublicKey
 }
 
 func clientErr(err error) error {
@@ -274,6 +276,16 @@ func (k *Key) Verify(data []byte, sig *ssh.Signature) error {
 	return pubKey.Verify(data, sig)
 }
 
+// CryptoPublicKey satisfies the crypto.PublicKey interface. This implementation
+// can return nil if the underlying public key is not valid or does not
+// implement the crypto.PublicKey interface.
+func (k *Key) CryptoPublicKey() crypto.PublicKey {
+	if k.publicKey != nil {
+		return k.publicKey
+	}
+	return nil
+}
+
 type wireKey struct {
 	Format string
 	Rest   []byte `ssh:"rest"`
@@ -295,11 +307,17 @@ func parseKey(in []byte) (out *Key, rest []byte, err error) {
 		return nil, nil, err
 	}
 
-	return &Key{
+	k := &Key{
 		Format:  wk.Format,
 		Blob:    record.Blob,
 		Comment: record.Comment,
-	}, record.Rest, nil
+	}
+	if pubKey, err := ssh.ParsePublicKey(k.Blob); err == nil {
+		if p, ok := pubKey.(crypto.PublicKey); ok {
+			k.publicKey = p
+		}
+	}
+	return k, record.Rest, nil
 }
 
 // client is a client for an ssh-agent process.
