dcdfa25455887bc1be7959bc4771a7155bf5e96b
Nicola Murino <nicola.murino@gmail.com>
2023-10-03 19:55:46 +0200

    ssh: add server side support for Diffie Hellman Group Exchange
    We add this support for the following reasons:
    
    - We are planning to expose recommended (secure) vs. supported (works,
      not necessarily recommended) algorithms. The DHGEX kex is currently
      only exposed as a client-side kex. To simplify the calling convention
      for this follow-on, we expose the server side too.
    - Some clients are quite inflexible with reference to kex algorithms
      choice, for example they offer:
    
      diffie-hellman-group-exchange-sha256, diffie-hellman-group-exchange-sha1,
      diffie-hellman-group14-sha1, diffie-hellman-group1-sha1
    
      therefore DHGEX helps interoperability.
    
    We do not recommend the DHGEX kex as a whole:
    
    - the negotiation requires an extra round trip
    - the server must generate parameters (slow) or hardcode them, which
      defeats the security benefit over traditional DH.
    
    In this implementation we hardcode sending Oakley Group 14 or Oakley
    Group 16.
    
    Users that are concerned with security of classical DH kex should
    migrate to kex based on EC or Ed25519.
    
    Change-Id: I127822e90efc36821af4aca679931f40a2023021

--- ssh/test/sshcli_test.go
+++ ssh/test/sshcli_test.go
@@ -93,3 +93,50 @@ func TestSSHCLIAuth(t *testing.T) {
 		t.Fatalf("user certificate authentication failed, error: %v, command output %q", err, string(out))
 	}
 }
+
+func TestServerDHGex(t *testing.T) {
+	sshCLI := getSSHClient(t)
+	dir := t.TempDir()
+	keyPrivPath := filepath.Join(dir, "rsa")
+
+	for fn, content := range map[string][]byte{
+		keyPrivPath:          testdata.PEMBytes["rsa"],
+		keyPrivPath + ".pub": ssh.MarshalAuthorizedKey(testPublicKeys["rsa"]),
+	} {
+		if err := os.WriteFile(fn, content, 0600); err != nil {
+			t.Fatalf("WriteFile(%q): %v", fn, err)
+		}
+	}
+
+	config := &ssh.ServerConfig{
+		Config: ssh.Config{
+			KeyExchanges: []string{"diffie-hellman-group-exchange-sha256", "diffie-hellman-group-exchange-sha1"},
+		},
+		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+			if conn.User() == "testpubkey" && bytes.Equal(key.Marshal(), testPublicKeys["rsa"].Marshal()) {
+				return nil, nil
+			}
+
+			return nil, fmt.Errorf("pubkey for %q not acceptable", conn.User())
+		},
+	}
+	config.AddHostKey(testSigners["rsa"])
+
+	server, err := newTestServer(config)
+	if err != nil {
+		t.Fatalf("unable to start test server: %v", err)
+	}
+	defer server.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	for _, kex := range []string{"diffie-hellman-group-exchange-sha256", "diffie-hellman-group-exchange-sha1"} {
+		cmd := exec.CommandContext(ctx, sshCLI, "-vvv", "-i", keyPrivPath, "-o", "StrictHostKeyChecking=no",
+			"-o", fmt.Sprintf("KexAlgorithms=%s", kex), "-p", server.port, "testpubkey@127.0.0.1", "true")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatalf("%s failed, error: %v, command output %q", kex, err, string(out))
+		}
+	}
+}
