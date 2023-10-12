18d4ca1612dcf4a4a28f103cafe8278dee75ae88
Kobe Housen <kobehousen@google.com>
2023-10-12 10:12:22 +0000

    ssh: allow dialing named services in addition to port numbers
    e.g. Dial("tcp", "host.domain:ssh")
    
    This will do service name to port number resolution on the client side.
    
    Change-Id: I79fc2987d240ac6ad39c5bf3b823e816433a5382
    GitHub-Last-Rev: 612be7106ac52c2425c31fa89c468be44ee7747a
    GitHub-Pull-Request: golang/crypto#235

--- ssh/tcpip_test.go
+++ ssh/tcpip_test.go
@@ -5,6 +5,7 @@
 package ssh
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -16,5 +17,74 @@ func TestAutoPortListenBroken(t *testing.T) {
 	}
 	if isBrokenOpenSSHVersion(works) {
 		t.Errorf("version %q marked as broken", works)
+	}
+}
+
+func TestDialNamedPort(t *testing.T) {
+	// Test that sshClient.Dial supports named ports.
+
+	srvConn, clientConn, err := netPipe()
+	if err != nil {
+		t.Fatalf("netPipe: %v", err)
+	}
+	defer srvConn.Close()
+	defer clientConn.Close()
+
+	serverConf := &ServerConfig{
+		NoClientAuth: true,
+	}
+	serverConf.AddHostKey(testSigners["rsa"])
+	srvErr := make(chan error, 10)
+	go func() {
+		defer close(srvErr)
+		_, chans, req, err := NewServerConn(srvConn, serverConf)
+		if err != nil {
+			srvErr <- fmt.Errorf("NewServerConn: %w", err)
+			return
+		}
+		go DiscardRequests(req)
+		for newChan := range chans {
+			if newChan.ChannelType() != "direct-tcpip" {
+				srvErr <- fmt.Errorf("expected direct-tcpip channel, got=%s", newChan.ChannelType())
+			}
+			data := channelOpenDirectMsg{}
+			if err := Unmarshal(newChan.ExtraData(), &data); err != nil {
+				srvErr <- err
+			}
+			// Below we dial for service `ssh` which should be translated to 22.
+			if data.Port != 22 {
+				srvErr <- fmt.Errorf("expected port 22 got=%d", data.Port)
+			}
+			ch, reqs, err := newChan.Accept()
+			if err != nil {
+				srvErr <- fmt.Errorf("Accept: %w", err)
+			}
+			go DiscardRequests(reqs)
+			ch.Close()
+		}
+	}()
+
+	clientConf := &ClientConfig{
+		User:            "testuser",
+		HostKeyCallback: InsecureIgnoreHostKey(),
+	}
+
+	sshClientConn, newChans, reqs, err := NewClientConn(clientConn, "", clientConf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sshClient := NewClient(sshClientConn, newChans, reqs)
+
+	// port being a named port `ssh` is the main point of the test.
+	tcpipconn, err := sshClient.Dial("tcp", "localhost:ssh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tcpipconn.Close()
+
+	clientConn.Close()
+
+	for err := range srvErr {
+		t.Errorf("ssh server: %s", err)
 	}
 }
