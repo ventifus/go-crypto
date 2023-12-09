d80e04fd8665fdec960f25ce2969291f71334d52
Nicola Murino <nicola.murino@gmail.com>
2023-12-09 14:25:29 +0100

    ssh: add server side support for Diffie Hellman Group Exchange
    We add this support for the following reasons:
    
    - We are planning to expose recommended (secure) vs. supported (works,
      not necessarily recommended) algorithms. The DHGEX kex is currently
      only exposed as a client-side kex. To simplify the calling convention
      for this follow-on, we expose the server side too.
    - Some clients are quite inflexible with reference to kex algorithms
      choice, for example they offer:
    
      diffie-hellman-group-exchange-sha256, diffie-hellman-group-exchange-sha1,
      diffie-hellman-group14-sha1, diffie-hellman-group1-sha1
    
      therefore DHGEX helps interoperability.
    
    We do not recommend the DHGEX kex as a whole:
    
    - the negotiation requires an extra round trip
    - the server must generate parameters (slow) or hardcode them, which
      defeats the security benefit over traditional DH.
    
    In this implementation we hardcode sending Oakley Group 14 or Oakley
    Group 16.
    
    Users that are concerned with security of classical DH kex should
    migrate to kex based on EC or Ed25519.
    
    Fixes golang/go#54743
    
    Change-Id: I127822e90efc36821af4aca679931f40a2023021

--- ssh/server.go
+++ ssh/server.go
@@ -217,12 +217,6 @@ func NewServerConn(c net.Conn, config *ServerConfig) (*ServerConn, <-chan NewCha
 			}
 		}
 	}
-	// Check if the config contains any unsupported key exchanges
-	for _, kex := range fullConf.KeyExchanges {
-		if _, ok := serverForbiddenKexAlgos[kex]; ok {
-			return nil, nil, nil, fmt.Errorf("ssh: unsupported key exchange %s for server", kex)
-		}
-	}
 
 	s := &connection{
 		sshConn: sshConn{conn: c},
