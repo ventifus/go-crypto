e21fde9ca92aa081c62bed8fa102a10048325aeb
Nicola Murino <nicola.murino@gmail.com>
2023-12-16 18:48:15 +0100

    ssh: add fipsonly mode
    Fixes golang/go#64769
    
    Change-Id: I4132438bc5586215661c2c1872b5a6c7464badf4

--- ssh/client_auth.go
+++ ssh/client_auth.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"io"
 	"strings"
+
+	"golang.org/x/crypto/ssh/internal/fips"
 )
 
 type authResult int
@@ -315,6 +317,18 @@ func (cb publicKeyCallback) auth(session []byte, user string, c packetConn, rand
 	origSignersLen := len(signers)
 	for idx := 0; idx < len(signers); idx++ {
 		signer := signers[idx]
+		if fips.Enabled {
+			if rsaKey, ok := signer.PublicKey().(*rsaPublicKey); ok {
+				if !isFIPSSupportedRSASize(rsaKey.N.BitLen()) {
+					continue
+				}
+			}
+			if ecdsaKey, ok := signer.PublicKey().(*ecdsaPublicKey); ok {
+				if !isFIPSSupportedECDSASize(ecdsaKey.Params().BitSize) {
+					continue
+				}
+			}
+		}
 		pub := signer.PublicKey()
 		as, algo, err := pickSignatureAlgorithm(signer, extensions)
 		if err != nil && errSigAlgo == nil {
