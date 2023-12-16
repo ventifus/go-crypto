84a921f43d81de52a4bdb06456bfa024adea439d
Nicola Murino <nicola.murino@gmail.com>
2023-12-16 18:48:15 +0100

    ssh: add fipsonly mode
    Fixes golang/go#64769
    
    Change-Id: I4132438bc5586215661c2c1872b5a6c7464badf4

---
+++ ssh/internal/fips/fips.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package fips allows to restrict all SSH configuration to FIPS-approved
+// settings.
+package fips
+
+// Enabled defines the FIPS status.
+var Enabled = false
