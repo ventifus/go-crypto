290e68c7bfc025523dc7e240e7c464704c44940d
Nicola Murino <nicola.murino@gmail.com>
2023-12-22 15:06:26 +0100

    ssh: implement ETM mode for AES CBC cipher
    Fixes golang/go#64779
    
    Change-Id: I110c1bcaaf075d858c70ba87b070f1d5718ecf4c

--- ssh/client_test.go
+++ ssh/client_test.go
@@ -365,3 +365,57 @@ func TestUnsupportedAlgorithm(t *testing.T) {
 		})
 	}
 }
+
+func TestAESCBC(t *testing.T) {
+	for _, tt := range []struct {
+		name   string
+		config Config
+	}{
+		{
+			"AES-CBC",
+			Config{
+				Ciphers: []string{"aes128-cbc"},
+				MACs:    []string{"hmac-sha2-256"},
+			},
+		},
+		{
+			"AES-CBC-ETM",
+			Config{
+				Ciphers: []string{"aes128-cbc"},
+				MACs:    []string{"hmac-sha2-256-etm@openssh.com"},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			c1, c2, err := netPipe()
+			if err != nil {
+				t.Fatalf("netPipe: %v", err)
+			}
+			defer c1.Close()
+			defer c2.Close()
+
+			serverConf := &ServerConfig{
+				Config: tt.config,
+				PasswordCallback: func(conn ConnMetadata, password []byte) (*Permissions, error) {
+					return &Permissions{}, nil
+				},
+			}
+			serverConf.AddHostKey(testSigners["rsa"])
+			go NewServerConn(c1, serverConf)
+
+			clientConf := &ClientConfig{
+				User:   "testuser",
+				Config: tt.config,
+				Auth: []AuthMethod{
+					Password("testpw"),
+				},
+				HostKeyCallback: InsecureIgnoreHostKey(),
+			}
+			_, _, _, err = NewClientConn(c2, "", clientConf)
+			if err != nil {
+				t.Errorf("%s: unexpected error %v", tt.name, err)
+
+			}
+		})
+	}
+}
