4a62627210a2ca3ff172723ffe183bb1b415e0aa
Nicola Murino <nicola.murino@gmail.com>
2024-02-11 15:53:39 +0100

    ssh: add deadlines support for channels (WIP)
    Change-Id: Ica42573cdf11ddf58e48b51fa82466a14cc5e606

--- ssh/mux_test.go
+++ ssh/mux_test.go
@@ -5,11 +5,13 @@
 package ssh
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"sync"
 	"testing"
+	"time"
 )
 
 func muxPair() (*mux, *mux) {
@@ -260,6 +262,52 @@ func TestMuxConnectionCloseWriteUnblock(t *testing.T) {
 
 	writer.remoteWin.waitWriterBlocked()
 	mux.Close()
+}
+
+func TestChannelWriteDeadlines(t *testing.T) {
+	r, w, mux := channelPair(t)
+	defer r.Close()
+	defer w.Close()
+	defer mux.Close()
+
+	writer, ok := w.Extended(0).(*extChannel)
+	if !ok {
+		t.Fatal("writer is not an extended channel")
+	}
+	writer.SetWriteDeadline(time.Now().Add(-1 * time.Second))
+
+	err := w.writePacket(make([]byte, channelWindowSize))
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("expected write error %v, got: %v", context.DeadlineExceeded, err)
+	}
+	w.writeMu.Lock()
+	if !w.writeDeadlineExceeded {
+		t.Error("write deadline exceed flag is not set")
+	}
+	w.writeMu.Unlock()
+	// a second write will return an error without trying to write
+	_, err = w.Write(make([]byte, channelWindowSize))
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("expected write error %v, got: %v", context.DeadlineExceeded, err)
+	}
+}
+
+func TestChannelReadDeadlines(t *testing.T) {
+	r, w, mux := channelPair(t)
+	defer r.Close()
+	defer w.Close()
+	defer mux.Close()
+
+	reader, ok := r.Extended(0).(*extChannel)
+	if !ok {
+		t.Fatal("reader is not an extended channel")
+	}
+	reader.SetReadDeadline(time.Now().Add(50 * time.Millisecond))
+
+	_, err := r.Read(make([]byte, channelWindowSize))
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("expected read error %v, got: %v", context.DeadlineExceeded, err)
+	}
 }
 
 func TestMuxReject(t *testing.T) {
