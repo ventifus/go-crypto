4696d1892d53cd922423dafee947058118e70c19
qiulaidongfeng <2645477756@qq.com>
2025-01-25 17:26:55 +0800

    blake2b: implement hash.XOF
    Fixes golang/go#69518
    
    Change-Id: Id9989ac9b28262df77017e97f985f67c1571c3ce

---
+++ blake2b/go125.go
@@ -0,0 +1,10 @@
+//go:build go1.25
+
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package blake2b
+
+import "hash"
+
+var _ hash.XOF = (*xof)(nil)
