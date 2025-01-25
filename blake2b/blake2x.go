9f89fe3d1ad59aa8af85bc70a104632b8ee2384e
qiulaidongfeng <2645477756@qq.com>
2025-01-25 17:26:55 +0800

    blake2b: implement hash.XOF
    DO NOT SUBMIT -- Wait CL 644235
    
    Fixes golang/go#69518
    
    Change-Id: Id9989ac9b28262df77017e97f985f67c1571c3ce

--- blake2b/blake2x.go
+++ blake2b/blake2x.go
@@ -7,11 +7,16 @@ package blake2b
 import (
 	"encoding/binary"
 	"errors"
+	"hash"
 	"io"
 )
 
+var _ hash.XOF = (*xof)(nil)
+
 // XOF defines the interface to hash functions that
 // support arbitrary-length output.
+//
+// New callers should prefer the standard library [hash.XOF].
 type XOF interface {
 	// Write absorbs more data into the hash's state. It panics if called
 	// after Read.
@@ -47,6 +52,8 @@ const maxOutputLength = (1 << 32) * 64
 //
 // A non-nil key turns the hash into a MAC. The key must between
 // zero and 32 bytes long.
+//
+// Can be safely interface-upgraded to [hash.XOF].
 func NewXOF(size uint32, key []byte) (XOF, error) {
 	if len(key) > Size {
 		return nil, errKeySize
@@ -91,6 +98,13 @@ func (x *xof) Write(p []byte) (n int, err error) {
 func (x *xof) Clone() XOF {
 	clone := *x
 	return &clone
+}
+
+func (x *xof) BlockSize() int {
+	if x.readMode {
+		return 0
+	}
+	return x.d.BlockSize()
 }
 
 func (x *xof) Reset() {
