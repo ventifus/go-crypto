45b7c2a38f696895e2950e2c1197609a56452d78
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-06-15 17:52:36 +0300

    sha3: forward to inbox implementation on Go 1.24
    Using the inbox implementation at "crypto/sha3" reduces duplicate code
    and helps with FIPS-240 compliance.
    
    The duplicate implementation is still used by the legacy Keccak
    algorithm, and thus cannot be completely excluded.
    
    Change-Id: I95c6b22bb4956a2932a0bce2c2c0b3b96a9104ee

--- sha3/sha3.go
+++ sha3/sha3.go
@@ -8,6 +8,7 @@ import (
 	"crypto/subtle"
 	"encoding/binary"
 	"errors"
+	"hash"
 	"unsafe"
 
 	"golang.org/x/sys/cpu"
@@ -241,4 +242,35 @@ func (d *state) UnmarshalBinary(b []byte) error {
 	d.state = state
 
 	return nil
+}
+
+const (
+	dsbyteSHA3   = 0b00000110
+	dsbyteKeccak = 0b00000001
+	dsbyteShake  = 0b00011111
+	dsbyteCShake = 0b00000100
+
+	// rateK[c] is the rate in bytes for Keccak[c] where c is the capacity in
+	// bits. Given the sponge size is 1600 bits, the rate is 1600 - c bits.
+	rateK256  = (1600 - 256) / 8
+	rateK448  = (1600 - 448) / 8
+	rateK512  = (1600 - 512) / 8
+	rateK768  = (1600 - 768) / 8
+	rateK1024 = (1600 - 1024) / 8
+)
+
+// NewLegacyKeccak256 creates a new Keccak-256 hash.
+//
+// Only use this function if you require compatibility with an existing cryptosystem
+// that uses non-standard padding. All other users should use New256 instead.
+func NewLegacyKeccak256() hash.Hash {
+	return &state{rate: rateK512, outputLen: 32, dsbyte: dsbyteKeccak}
+}
+
+// NewLegacyKeccak512 creates a new Keccak-512 hash.
+//
+// Only use this function if you require compatibility with an existing cryptosystem
+// that uses non-standard padding. All other users should use New512 instead.
+func NewLegacyKeccak512() hash.Hash {
+	return &state{rate: rateK1024, outputLen: 64, dsbyte: dsbyteKeccak}
 }
