4341ee5a861e037374278f1130ba77d2f48941a3
Theodore Tsirpanis <teo@tsirpanis.gr>
2025-06-15 17:51:17 +0300

    pbkdf: forward to inbox implementation on Go 1.24
    Using the inbox implementation at "crypto/pbkdf2" reduces duplicate code
    and helps with FIPS-240 compliance.
    
    Change-Id: Ib35819b8fb2d3a180ebe9675bbb810bceb6796e7

--- pbkdf2/pbkdf2_1.24.go
+++ pbkdf2/pbkdf2_1.24.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !go1.24
+//go:build go1.24
 
 /*
 Package pbkdf2 implements the key derivation function PBKDF2 as defined in RFC
@@ -18,11 +18,10 @@ Hash Functions SHA-1, SHA-224, SHA-256, SHA-384 and SHA-512 for HMAC. To
 choose, you can pass the `New` functions from the different SHA packages to
 pbkdf2.Key.
 */
-
 package pbkdf2
 
 import (
-	"crypto/hmac"
+	"crypto/pbkdf2"
 	"hash"
 )
 
@@ -43,38 +42,9 @@ import (
 // Using a higher iteration count will increase the cost of an exhaustive
 // search but will also make derivation proportionally slower.
 func Key(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
-	prf := hmac.New(h, password)
-	hashLen := prf.Size()
-	numBlocks := (keyLen + hashLen - 1) / hashLen
-
-	var buf [4]byte
-	dk := make([]byte, 0, numBlocks*hashLen)
-	U := make([]byte, hashLen)
-	for block := 1; block <= numBlocks; block++ {
-		// N.B.: || means concatenation, ^ means XOR
-		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
-		// U_1 = PRF(password, salt || uint(i))
-		prf.Reset()
-		prf.Write(salt)
-		buf[0] = byte(block >> 24)
-		buf[1] = byte(block >> 16)
-		buf[2] = byte(block >> 8)
-		buf[3] = byte(block)
-		prf.Write(buf[:4])
-		dk = prf.Sum(dk)
-		T := dk[len(dk)-hashLen:]
-		copy(U, T)
-
-		// U_n = PRF(password, U_(n-1))
-		for n := 2; n <= iter; n++ {
-			prf.Reset()
-			prf.Write(U)
-			U = U[:0]
-			U = prf.Sum(U)
-			for x := range U {
-				T[x] ^= U[x]
-			}
-		}
+	ret, err := pbkdf2.Key(h, string(password), salt, iter, keyLen)
+	if err != nil {
+		panic(err)
 	}
-	return dk[:keyLen]
+	return ret
 }
